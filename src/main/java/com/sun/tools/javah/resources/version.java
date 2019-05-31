9be728e4d278bff3b4c8f45b4a0f2529ea807e16
Watts Ch <admin@wattsch.cn>
2019-05-31 16:59:14 +0800

    init

---
+++ src/main/java/com/sun/tools/javah/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javah.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0_212-8u212-b03-0ubuntu1.18.10.1-b03" },
+            { "jdk", "1.8.0_212" },
+            { "release", "1.8.0_212" },
+        };
+    }
+}
