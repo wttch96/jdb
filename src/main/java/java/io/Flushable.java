ac53eab9539a2e34a3118f101e065f31b14455ea
watts-home <admin@wattsch.cn>
2019-06-25 23:36:48 +0800

    java.io.Flushable

--- src/main/java/java/io/Flushable.java
+++ src/main/java/java/io/Flushable.java
@@ -1,46 +1,16 @@
-/*
- * Copyright (c) 2004, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.io;
 
-import java.io.IOException;
-
 /**
- * A <tt>Flushable</tt> is a destination of data that can be flushed.  The
- * flush method is invoked to write any buffered output to the underlying
- * stream.
+ *  <tt>Flushable</tt> 是指目标数据可以被刷新. 调用flush方法将任何缓冲输出写入底层流.
  *
  * @since 1.5
  */
 public interface Flushable {
 
     /**
-     * Flushes this stream by writing any buffered output to the underlying
-     * stream.
+     * 通过将任何缓冲输出写入底层流来刷新此流.
      *
-     * @throws IOException If an I/O error occurs
+     * @throws IOException 发生 I/O 异常
      */
     void flush() throws IOException;
 }
