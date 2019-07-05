a08902da7ce9745e4fc939da3d8b8683a4634263
Wttch <admin@wttch.com>
2019-07-05 17:53:16 +0800

    java.lang.annotation.ElementType;

--- src/main/java/java/lang/annotation/ElementType.java
+++ src/main/java/java/lang/annotation/ElementType.java
@@ -1,69 +1,33 @@
-/*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.annotation;
 
 /**
- * The constants of this enumerated type provide a simple classification of the
- * syntactic locations where annotations may appear in a Java program. These
- * constants are used in {@link Target java.lang.annotation.Target}
- * meta-annotations to specify where it is legal to write annotations of a
- * given type.
+ * 此枚举类型的常量提供了注解可能出现在 java 程序中的语法位置的简单分类.
+ * 这些常量用于 {@link Target java.lang.annotation.Targe} 元注解, 标注
+ * 给定类型可以添加注解的合法位置.
  *
- * <p>The syntactic locations where annotations may appear are split into
- * <em>declaration contexts</em> , where annotations apply to declarations, and
- * <em>type contexts</em> , where annotations apply to types used in
- * declarations and expressions.
+ * <p>注解可能出现的语法位置分为适用于声明的<em>声明上下文</em>, 适用于
+ * 声明和表达式的 <em>类型上下文</em>.
  *
- * <p>The constants {@link #ANNOTATION_TYPE} , {@link #CONSTRUCTOR} , {@link
- * #FIELD} , {@link #LOCAL_VARIABLE} , {@link #METHOD} , {@link #PACKAGE} ,
- * {@link #PARAMETER} , {@link #TYPE} , and {@link #TYPE_PARAMETER} correspond
- * to the declaration contexts in JLS 9.6.4.1.
+ * <p>常量 {@link #ANNOTATION_TYPE} , {@link #CONSTRUCTOR} , {@link #FIELD} ,
+ * {@link #LOCAL_VARIABLE} , {@link #METHOD} , {@link #PACKAGE} , {@link #PARAMETER} ,
+ * {@link #TYPE} , and {@link #TYPE_PARAMETER} 对应声明上下文,
+ * 于java语言规范第9.6.4.1节.
  *
- * <p>For example, an annotation whose type is meta-annotated with
- * {@code @Target(ElementType.FIELD)} may only be written as a modifier for a
- * field declaration.
+ * <p>举个例子, 一个注解类型被元注解 {@code @Target(ElementType.FIELD)}
+ * 只能写为字段声明的修饰符.
  *
- * <p>The constant {@link #TYPE_USE} corresponds to the 15 type contexts in JLS
- * 4.11, as well as to two declaration contexts: type declarations (including
- * annotation type declarations) and type parameter declarations.
+ * <p>常量 {@link #TYPE_USE} 对应 JLS 4.11 中的 15 种类型上下文,
+ * 以及两个声明上下文: 类型声明(包括注解类型声明)和类型参数声明.
  *
- * <p>For example, an annotation whose type is meta-annotated with
- * {@code @Target(ElementType.TYPE_USE)} may be written on the type of a field
- * (or within the type of the field, if it is a nested, parameterized, or array
- * type), and may also appear as a modifier for, say, a class declaration.
+ * <p>举个例子, 一个注解类型被元注解 {@code @Target(ElementType.TYPE_USE)} 标注
+ * 可以写在字段的类型上(或者在字段的类型内, 如果它是嵌套的, 参数化的或数组类型),
+ * 也可以作为类声明的修饰符出现.
  *
- * <p>The {@code TYPE_USE} constant includes type declarations and type
- * parameter declarations as a convenience for designers of type checkers which
- * give semantics to annotation types. For example, if the annotation type
- * {@code NonNull} is meta-annotated with
- * {@code @Target(ElementType.TYPE_USE)}, then {@code @NonNull}
- * {@code class C {...}} could be treated by a type checker as indicating that
- * all variables of class {@code C} are non-null, while still allowing
- * variables of other classes to be non-null or not non-null based on whether
- * {@code @NonNull} appears at the variable's declaration.
+ * <p> {@code TYPE_USE} 常量包含类型声明类型参数声明作为类型检查器设计者的边里,
+ * 如果一个注解类型 {@code NonNull} 被源主机 {@code @Target(ElementType.TYPE_USE)}
+ * 标注, 那么 {@code @NonNull} {@code class C {...}} 可以通过类型检查来表明所有
+ * {@code C} 的对象是非空的, 同时仍然允许其他类的变量为非null或非null,
+ * 这取决于{@code @NonNull}是否出现在变量的声明中.
  *
  * @author Joshua Bloch
  * @jls 9.6.4.1 @Target
@@ -72,54 +36,55 @@ package java.lang.annotation;
  */
 public enum ElementType {
     /**
-     * Class, interface (including annotation type), or enum declaration
+     * 声明 类, 接口(包含注解类型)或者枚举
      */
     TYPE,
 
     /**
-     * Field declaration (includes enum constants)
+     * 声明字段(包含枚举常量)
      */
     FIELD,
 
     /**
-     * Method declaration
+     * 声明方法
      */
     METHOD,
 
     /**
-     * Formal parameter declaration
+     * 声明形参
      */
     PARAMETER,
 
     /**
-     * Constructor declaration
+     * 声明构造器
      */
     CONSTRUCTOR,
 
     /**
-     * Local variable declaration
+     * 声明局部变量
      */
     LOCAL_VARIABLE,
 
     /**
-     * Annotation type declaration
+     * 声明注解类型
      */
     ANNOTATION_TYPE,
 
     /**
-     * Package declaration
+     * 声明包(在 package-info.java 文件 package 声明的位置使用)
      */
     PACKAGE,
 
     /**
-     * Type parameter declaration
+     * 声明自定义类型
+     * 声明泛型时使用
      *
      * @since 1.8
      */
     TYPE_PARAMETER,
 
     /**
-     * Use of a type
+     * 使用在声明类型的任意语句中
      *
      * @since 1.8
      */
