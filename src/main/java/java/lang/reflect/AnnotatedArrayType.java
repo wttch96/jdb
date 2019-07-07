ff0b11cbc6ab296a26252855eb58c2d1694a37b9
watts-home <admin@wattsch.cn>
2019-07-08 01:19:53 +0800

    java.lang.reflect.AnnotatedTypeVariable;

--- src/main/java/java/lang/reflect/AnnotatedArrayType.java
+++ src/main/java/java/lang/reflect/AnnotatedArrayType.java
@@ -2,16 +2,16 @@ package java.lang.reflect;
 
 
 /**
- * {@code AnnotatedArrayType} 表示数组类型的潜在注解用法, 其组件类型本身可以表示类型的注释用法.
+ * {@code AnnotatedArrayType} 表示数组类型的潜在注解用法, 其组件类型本身可以表示类型的注解用法.
  *
  * @since 1.8
  */
 public interface AnnotatedArrayType extends AnnotatedType {
 
     /**
-     * 返回此数组类型的可能带注释的通用组件类型.
+     * 返回此数组类型的可能带注解的通用组件类型.
      *
-     * @return 此数组类型的可能带注释的通用组件类型
+     * @return 此数组类型的可能带注解的通用组件类型
      */
     AnnotatedType getAnnotatedGenericComponentType();
 }
--- src/main/java/java/lang/reflect/AnnotatedParameterizedType.java
+++ src/main/java/java/lang/reflect/AnnotatedParameterizedType.java
@@ -2,16 +2,16 @@ package java.lang.reflect;
 
 /**
  * 被注解的参数化类型.
- * {@code AnnotatedParameterizedType} 表示参数化类型的潜在注释用法,
- * 其类型参数本身可以表示类型的注释用法.
+ * {@code AnnotatedParameterizedType} 表示参数化类型的潜在注解用法,
+ * 其类型参数本身可以表示类型的注解用法.
  *
  * @since 1.8
  */
 public interface AnnotatedParameterizedType extends AnnotatedType {
     /**
-     * 返回此参数化类型的可能带注释的实际类型参数.
+     * 返回此参数化类型的可能带注解的实际类型参数.
      *
-     * @return 此参数化类型的可能带注释的实际类型参数
+     * @return 此参数化类型的可能带注解的实际类型参数
      */
     AnnotatedType[] getAnnotatedActualTypeArguments();
 }
