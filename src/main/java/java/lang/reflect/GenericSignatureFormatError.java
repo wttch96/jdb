6ff2eff33e9b3663dc31aa453f888a6fa1b9242e
watts-home <admin@wattsch.cn>
2019-07-08 00:06:01 +0800

    java.lang.reflect.GenericSignatureFormatError;

--- src/main/java/java/lang/reflect/GenericSignatureFormatError.java
+++ src/main/java/java/lang/reflect/GenericSignatureFormatError.java
@@ -1,35 +1,8 @@
-/*
- * Copyright (c) 2003, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 
 /**
- * Thrown when a syntactically malformed signature attribute is
- * encountered by a reflective method that needs to interpret the
- * generic signature information for a type, method or constructor.
+ * 当需要解释类型, 方法或构造函数的通用签名信息的反射方法遇到语法错误的签名属性时抛出.
  *
  * @since 1.5
  */
@@ -37,17 +10,16 @@ public class GenericSignatureFormatError extends ClassFormatError {
     private static final long serialVersionUID = 6709919147137911034L;
 
     /**
-     * Constructs a new {@code GenericSignatureFormatError}.
+     * 构造一个新的 {@code GenericSignatureFormatError}.
      */
     public GenericSignatureFormatError() {
         super();
     }
 
     /**
-     * Constructs a new {@code GenericSignatureFormatError} with the
-     * specified message.
+     * 利用指定的消息构造一个新的{@code GenericSignatureFormatError}
      *
-     * @param message the detail message, may be {@code null}
+     * @param message 详细消息, 可能为 {@code null}
      */
     public GenericSignatureFormatError(String message) {
         super(message);
