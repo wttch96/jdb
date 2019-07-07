dd51f71b4a01a8173389c21da1c93b25858e7f8b
watts-home <admin@wattsch.cn>
2019-07-08 01:11:38 +0800

    java.lang.reflect.AnnotatedParameterizedType;

--- src/main/java/java/lang/reflect/AnnotatedParameterizedType.java
+++ src/main/java/java/lang/reflect/AnnotatedParameterizedType.java
@@ -1,43 +1,17 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * {@code AnnotatedParameterizedType} represents the potentially annotated use
- * of a parameterized type, whose type arguments may themselves represent
- * annotated uses of types.
+ * 被注解的参数化类型.
+ * {@code AnnotatedParameterizedType} 表示参数化类型的潜在注释用法,
+ * 其类型参数本身可以表示类型的注释用法.
  *
  * @since 1.8
  */
 public interface AnnotatedParameterizedType extends AnnotatedType {
-
     /**
-     * Returns the potentially annotated actual type arguments of this parameterized type.
+     * 返回此参数化类型的可能带注释的实际类型参数.
      *
-     * @return the potentially annotated actual type arguments of this parameterized type
+     * @return 此参数化类型的可能带注释的实际类型参数
      */
     AnnotatedType[] getAnnotatedActualTypeArguments();
 }
