3dd796be2f3345dc0506b62eea256b1da5cff45f
watts-home <admin@wattsch.cn>
2019-07-07 13:41:10 +0800

    java.lang.reflect.Type;

--- src/main/java/java/lang/reflect/Type.java
+++ src/main/java/java/lang/reflect/Type.java
@@ -1,47 +1,21 @@
-/*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * Type is the common superinterface for all types in the Java
- * programming language. These include raw types, parameterized types,
- * array types, type variables and primitive types.
+ * Type是所有Java编程语言类型的共同超类接口. 它们包含原始类型, 参数化类型,
+ * 数组类型, 类型变量和基本类型.
  *
  * @since 1.5
  */
 public interface Type {
     /**
-     * Returns a string describing this type, including information
-     * about any type parameters.
+     * 返回一个字符串来描述这个类型, 包含任何有关类型参数的信息.
      *
-     * @return a string describing this type
-     * @implSpec The default implementation calls {@code toString}.
+     * @return 一个字符串来描述这个类型
+     * @implSpec 默认实现是调用 {@code toString}.
      * @since 1.8
      */
     default String getTypeName() {
+
         return toString();
     }
 }
