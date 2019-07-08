308324323fa10d5d3a183da3a7d652f1f7336ce0
Wttch <admin@wttch.com>
2019-07-08 18:34:07 +0800

    java.lang.reflect.Member;

--- src/main/java/java/lang/reflect/Member.java
+++ src/main/java/java/lang/reflect/Member.java
@@ -1,33 +1,7 @@
-/*
- * Copyright (c) 1996, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * Member is an interface that reflects identifying information about
- * a single member (a field or a method) or a constructor.
+ * Member是反映单个成员(字段或方法)或构造函数的标识信息的接口.
  *
  * @author Nakul Saraiya
  * @see java.lang.Class
@@ -39,39 +13,33 @@ public
 interface Member {
 
     /**
-     * Identifies the set of all public members of a class or interface,
-     * including inherited members.
+     * 标示类或接口的所有公共成员的集合, 包括继承的成员.
      */
     public static final int PUBLIC = 0;
 
     /**
-     * Identifies the set of declared members of a class or interface.
-     * Inherited members are not included.
+     * 标示类或接口的已声明成员集合.
+     * 不包括继承的成员.
      */
     public static final int DECLARED = 1;
 
     /**
-     * Returns the Class object representing the class or interface
-     * that declares the member or constructor represented by this Member.
+     * 返回表示声明由此 Member 表示的成员或构造函数的类或接口的 Class 对象.
      *
-     * @return an object representing the declaring class of the
-     * underlying member
+     * @return 表示底层成员的声明类的对象
      */
     public Class<?> getDeclaringClass();
 
     /**
-     * Returns the simple name of the underlying member or constructor
-     * represented by this Member.
+     * 返回此 Member 表示的基础成员或构造函数的简单名称.
      *
-     * @return the simple name of the underlying member
+     * @return 基础成员的简单名称
      */
     public String getName();
 
     /**
-     * Returns the Java language modifiers for the member or
-     * constructor represented by this Member, as an integer.  The
-     * Modifier class should be used to decode the modifiers in
-     * the integer.
+     * 返回这个成员或者构造器的 java 语言修饰符(int 类型).
+     * 应使用 Modifier 类来解码整数中的修饰符.
      *
      * @return the Java language modifiers for the underlying member
      * @see Modifier
@@ -79,11 +47,9 @@ interface Member {
     public int getModifiers();
 
     /**
-     * Returns {@code true} if this member was introduced by
-     * the compiler; returns {@code false} otherwise.
+     * 返回 {@code true} 如果这个成员是由编译器引入的, 否则 {@code false}.
      *
-     * @return true if and only if this member was introduced by
-     * the compiler.
+     * @return true 如果这个成员是由编译器引入的
      * @jls 13.1 The Form of a Binary
      * @since 1.5
      */
