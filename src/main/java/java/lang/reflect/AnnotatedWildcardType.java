d8f4d919651621ea3c72ca17bb1a6cb97545262f
Wttch <admin@wttch.com>
2019-07-08 12:30:45 +0800

    java.lang.reflect.AnnotatedWildcardType;

--- src/main/java/java/lang/reflect/AnnotatedWildcardType.java
+++ src/main/java/java/lang/reflect/AnnotatedWildcardType.java
@@ -1,50 +1,24 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * {@code AnnotatedWildcardType} represents the potentially annotated use of a
- * wildcard type argument, whose upper or lower bounds may themselves represent
- * annotated uses of types.
+ * {@code AnnotatedWildcardType} 表示通配符类型参数的潜在注解用法,
+ * 其上限或下限本身可以表示类型的注解用法。
  *
  * @since 1.8
  */
 public interface AnnotatedWildcardType extends AnnotatedType {
 
     /**
-     * Returns the potentially annotated lower bounds of this wildcard type.
+     * 返回此通配符类型的可能带注解的下限.
      *
-     * @return the potentially annotated lower bounds of this wildcard type
+     * @return 此通配符类型的可能带注解的下限.
      */
     AnnotatedType[] getAnnotatedLowerBounds();
 
     /**
-     * Returns the potentially annotated upper bounds of this wildcard type.
+     * 返回此通配符类型的可能带注解的上限.
      *
-     * @return the potentially annotated upper bounds of this wildcard type
+     * @return 此通配符类型的可能带注解的上限.
      */
     AnnotatedType[] getAnnotatedUpperBounds();
 }
