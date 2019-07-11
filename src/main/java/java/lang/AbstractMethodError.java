f67e756a367d00d60f9232cebea5398132efd29d
Wttch <admin@wttch.com>
2019-07-11 14:11:37 +0800

    java.lang.AbstractMethodError;

--- src/main/java/java/lang/AbstractMethodError.java
+++ src/main/java/java/lang/AbstractMethodError.java
@@ -1,36 +1,9 @@
-/*
- * Copyright (c) 1994, 2008, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * Thrown when an application tries to call an abstract method.
- * Normally, this error is caught by the compiler; this error can
- * only occur at run time if the definition of some class has
- * incompatibly changed since the currently executing method was last
- * compiled.
+ * 当应用程序尝试调用抽象方法时抛出这个异常.
+ * 通常这个错误被编译器捕获;
+ * 如果自上次编译当前正在执行的方法以来某些类的定义发生了不兼容的更改, 则次错误只能在运行时发生.
  *
  * @author unascribed
  * @since JDK1.0
@@ -40,17 +13,16 @@ class AbstractMethodError extends IncompatibleClassChangeError {
     private static final long serialVersionUID = -1654391082989018462L;
 
     /**
-     * Constructs an <code>AbstractMethodError</code> with no detail  message.
+     * 构造一个 <code>AbstractMethodError</code> 不使用任何消息.
      */
     public AbstractMethodError() {
         super();
     }
 
     /**
-     * Constructs an <code>AbstractMethodError</code> with the specified
-     * detail message.
+     * 用指定的消息构造一个 <code>AbstractMethodError</code> 对象.
      *
-     * @param s the detail message.
+     * @param s 详细的消息
      */
     public AbstractMethodError(String s) {
         super(s);
