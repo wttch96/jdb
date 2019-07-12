021a5c28a537d402682db021bc41754e23deed5c
Wttch <admin@wttch.com>
2019-07-12 16:54:21 +0800

    java.io.IOException;

--- src/main/java/java/io/IOException.java
+++ src/main/java/java/io/IOException.java
@@ -1,80 +1,40 @@
-/*
- * Copyright (c) 1994, 2006, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.io;
 
 /**
- * Signals that an I/O exception of some sort has occurred. This
- * class is the general class of exceptions produced by failed or
- * interrupted I/O operations.
+ * 表示发生某种 I/O 异常的信号. 此类是由失败或中断的 I/O 操作产生的一般异常类.
  *
- * @author  unascribed
- * @see     java.io.InputStream
- * @see     java.io.OutputStream
- * @since   JDK1.0
+ * @author unascribed
+ * @see java.io.InputStream
+ * @see java.io.OutputStream
+ * @since JDK1.0
  */
 public
 class IOException extends Exception {
     static final long serialVersionUID = 7818375828146090155L;
 
     /**
-     * Constructs an {@code IOException} with {@code null}
-     * as its error detail message.
+     * 使用 {@code null} 构造一个 {@code IOException} 作为其错误详细消息.
      */
     public IOException() {
         super();
     }
 
     /**
-     * Constructs an {@code IOException} with the specified detail message.
+     * 使用指定的错误详细消息构造一个 {@code IOException} 异常.
      *
-     * @param message
-     *        The detail message (which is saved for later retrieval
-     *        by the {@link #getMessage()} method)
+     * @param message 详细消息 (保存之后可以使用 {@link #getMessage()} 方法获得)
      */
     public IOException(String message) {
         super(message);
     }
 
     /**
-     * Constructs an {@code IOException} with the specified detail message
-     * and cause.
+     * 使用指定的消息和原因构造一个　{@code IOException} 异常.
      *
-     * <p> Note that the detail message associated with {@code cause} is
-     * <i>not</i> automatically incorporated into this exception's detail
-     * message.
+     * <p> 请注意, 与 {@code cause} 关联的详细消息 <i>不</i>会自动合并到此异常的详细信息中.
      *
-     * @param message
-     *        The detail message (which is saved for later retrieval
-     *        by the {@link #getMessage()} method)
-     *
-     * @param cause
-     *        The cause (which is saved for later retrieval by the
-     *        {@link #getCause()} method).  (A null value is permitted,
-     *        and indicates that the cause is nonexistent or unknown.)
-     *
+     * @param message 详细消息 (保存之后可以使用 {@link #getMessage()} 方法获得)
+     * @param cause   原因 (可以稍候使用 {@link #getCause()} 方法恢复).  (允许空值, 表示原因不存在或未知.)
      * @since 1.6
      */
     public IOException(String message, Throwable cause) {
@@ -82,17 +42,11 @@ class IOException extends Exception {
     }
 
     /**
-     * Constructs an {@code IOException} with the specified cause and a
-     * detail message of {@code (cause==null ? null : cause.toString())}
-     * (which typically contains the class and detail message of {@code cause}).
-     * This constructor is useful for IO exceptions that are little more
-     * than wrappers for other throwables.
+     * 构造一个　{@code IOException}, 其中包括　{@code (cause == null?null:cause.toString() } 的指定原因
+     * 和详细消息(通常包含 {@code cause} 的类和详细消息). 次构造函数对于 IO 异常非常有用, 这些异常只是其他
+     * Throwable 的包装器.
      *
-     * @param cause
-     *        The cause (which is saved for later retrieval by the
-     *        {@link #getCause()} method).  (A null value is permitted,
-     *        and indicates that the cause is nonexistent or unknown.)
-     *
+     * @param cause 原因 (可以稍候使用 {@link #getCause()} 方法恢复).  (允许空值, 表示原因不存在或未知.)
      * @since 1.6
      */
     public IOException(Throwable cause) {
