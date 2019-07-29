73bb13d1f088ceef8faa4056afa690b113ab5de1
Wttch <admin@wttch.com>
2019-07-29 17:55:25 +0800

    BufferedInputStream 完结撒花 ❁❁❁

--- src/main/java/java/io/BufferedInputStream.java
+++ src/main/java/java/io/BufferedInputStream.java
@@ -54,45 +54,28 @@ class BufferedInputStream extends FilterInputStream {
     protected int count;
 
     /**
-     * The current position in the buffer. This is the index of the next
-     * character to be read from the <code>buf</code> array.
+     * 缓冲区的当前位置. 下一个将从 <code>buf</code> 数组中读取的字节的下标索引.
      * <p>
-     * This value is always in the range <code>0</code>
-     * through <code>count</code>. If it is less
-     * than <code>count</code>, then  <code>buf[pos]</code>
-     * is the next byte to be supplied as input;
-     * if it is equal to <code>count</code>, then
-     * the  next <code>read</code> or <code>skip</code>
-     * operation will require more bytes to be
-     * read from the contained  input stream.
+     * 该值始终在 <code>0</code> 到 <code>count</code> 范围之间. 如果它小于
+     * <code>count</code>, 则 <code>buf[pos]</code>将作为输入下一个提供的字符;
+     * 如果等于 <code>count</code>, 那么下一次 <code>read</code> 或 <code>skip</code>
+     * 操作将需要从包含的输入流中读取更多的字节.
      *
      * @see java.io.BufferedInputStream#buf
      */
     protected int pos;
 
     /**
-     * The value of the <code>pos</code> field at the time the last
-     * <code>mark</code> method was called.
+     * 上一次调用 <code>mark</code> 方法时 <code>pos</code> 字段的值.
      * <p>
-     * This value is always
-     * in the range <code>-1</code> through <code>pos</code>.
-     * If there is no marked position in  the input
-     * stream, this field is <code>-1</code>. If
-     * there is a marked position in the input
-     * stream,  then <code>buf[markpos]</code>
-     * is the first byte to be supplied as input
-     * after a <code>reset</code> operation. If
-     * <code>markpos</code> is not <code>-1</code>,
-     * then all bytes from positions <code>buf[markpos]</code>
-     * through  <code>buf[pos-1]</code> must remain
-     * in the buffer array (though they may be
-     * moved to  another place in the buffer array,
-     * with suitable adjustments to the values
-     * of <code>count</code>,  <code>pos</code>,
-     * and <code>markpos</code>); they may not
-     * be discarded unless and until the difference
-     * between <code>pos</code> and <code>markpos</code>
-     * exceeds <code>marklimit</code>.
+     * 该值始终在 <code>-1</code> 和 <code>pos</code> 范围之间.
+     * 如果输入流没有标记位置, 该字段是 <code>-1</code>.
+     * 如果输入流有标记位置, 那么 <code>buf[markpos]</code> 将作为 <code>reset</code>
+     * 方法调用之后输入流提供的第一个字节. 如果 <code>markpos</code> 不是 <code>-1</code>,
+     * 那么来自位置 <code>buf[markpos]</code> 到 <code>buf[pos-1]</code> 的所有字节
+     * 必须保留在缓冲区数组中(尽管可以将他们移动到缓冲区数组中的另一个位置, 并适当调整 <code>count</code>,
+     * <code>pos</code> 和 <code>markpos</code> 的值); 除非直到 <code>pos</code> 和 <code>markpos</code>
+     * 之间的差异超过<code>marklimit</code>, 否则它们不会被丢弃.
      *
      * @see java.io.BufferedInputStream#mark(int)
      * @see java.io.BufferedInputStream#pos
@@ -100,13 +83,10 @@ class BufferedInputStream extends FilterInputStream {
     protected int markpos = -1;
 
     /**
-     * The maximum read ahead allowed after a call to the
-     * <code>mark</code> method before subsequent calls to the
-     * <code>reset</code> method fail.
-     * Whenever the difference between <code>pos</code>
-     * and <code>markpos</code> exceeds <code>marklimit</code>,
-     * then the  mark may be dropped by setting
-     * <code>markpos</code> to <code>-1</code>.
+     * 在后续调用 <code>reset</code> 方法失败之前调用 <code>mark</code>
+     * 方法后允许的最大预读. 只要 <code>pos</code> 和 <code>markpos</code>
+     * 之间的差异超过 <code>marklimit</code>, 就可以通过将 <code>markpos</code>
+     * 设置为 <code>-1</code> 来删除标记.
      *
      * @see java.io.BufferedInputStream#mark(int)
      * @see java.io.BufferedInputStream#reset()
@@ -136,27 +116,23 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * Creates a <code>BufferedInputStream</code>
-     * and saves its  argument, the input stream
-     * <code>in</code>, for later use. An internal
-     * buffer array is created and  stored in <code>buf</code>.
+     * 创建 <code>BufferedInputStream</code> 并保存其参数,
+     * 即输入流 <code>in</code> , 供以后使用. 创建内部缓冲区数组
+     * 并将其存储在 <code>buf</code> 中.
      *
-     * @param in the underlying input stream.
+     * @param in 底层输入流.
      */
     public BufferedInputStream(InputStream in) {
         this(in, DEFAULT_BUFFER_SIZE);
     }
 
     /**
-     * Creates a <code>BufferedInputStream</code>
-     * with the specified buffer size,
-     * and saves its  argument, the input stream
-     * <code>in</code>, for later use.  An internal
-     * buffer array of length  <code>size</code>
-     * is created and stored in <code>buf</code>.
+     * 使用指定的缓冲大小创建 <code>BufferedInputStream</code>
+     * 并保存其参数, 即输入流 <code>in</code>, 供以后使用.
+     * 一个 <code>size</code> 长度的内部缓冲数组并将其存储在 <code>buf</code> 中.
      *
-     * @param in   the underlying input stream.
-     * @param size the buffer size.
+     * @param in   底层输入流.
+     * @param size 缓冲大小.
      * @throws IllegalArgumentException if {@code size <= 0}.
      */
     public BufferedInputStream(InputStream in, int size) {
@@ -168,47 +144,53 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * Fills the buffer with more data, taking into account
-     * shuffling and other tricks for dealing with marks.
-     * Assumes that it is being called by a synchronized method.
-     * This method also assumes that all data has already been read in,
-     * hence pos > count.
+     * 使用更多数组填充缓冲区, 考虑到用于处理标记的混洗和其他技巧.
+     * 假设它是由同步方法调用的. 此方法还假定已读取 in 的所有数据,
+     * 因此 pos > count.
      */
     private void fill() throws IOException {
         byte[] buffer = getBufIfOpen();
         if (markpos < 0) {
-            pos = 0;            /* no mark: throw away the buffer */
-        } else if (pos >= buffer.length)  /* no room left in buffer */ {
-            if (markpos > 0) {  /* can throw away early part of the buffer */
+            pos = 0;            /* 没有标记: 扔掉缓冲区 */
+        } else if (pos >= buffer.length)  /* 没有留在缓冲区的空间 */ {
+            if (markpos > 0) {  /* 可以扔掉早期的缓冲区 */
+                // 扔掉早期的缓冲区, 将 markpos 到 pos 的缓冲复制到 0 到 (pos - markpos) 的位置
                 int sz = pos - markpos;
                 System.arraycopy(buffer, markpos, buffer, 0, sz);
                 pos = sz;
                 markpos = 0;
             } else if (buffer.length >= marklimit) {
-                markpos = -1;   /* buffer got too big, invalidate mark */
-                pos = 0;        /* drop buffer contents */
+                // pos >= buffer.length >= marklimit, pos 已经超过了 marklimit 限制, 标记无效
+                markpos = -1;   /* 缓冲区太大, 标记无效 */
+                pos = 0;        /* 删除缓冲区内容 */
             } else if (buffer.length >= MAX_BUFFER_SIZE) {
+                // 大于允许的最大缓冲区大小
                 throw new OutOfMemoryError("Required array size too large");
-            } else {            /* grow buffer */
+            } else {            /* 增加缓冲区大小 */
+                // markpos <= 0 && buffer.length < marklimit && buffer.length < MAX_BUFFER_SIZE
+                // 倍增缓冲区大小, 最大 MAX_BUFFER_SIZE
                 int nsz = (pos <= MAX_BUFFER_SIZE - pos) ?
                         pos * 2 : MAX_BUFFER_SIZE;
+                // 最大只到 marklimit
                 if (nsz > marklimit) {
                     nsz = marklimit;
                 }
                 byte nbuf[] = new byte[nsz];
+                // 拷贝之前缓冲到新的缓冲区去
                 System.arraycopy(buffer, 0, nbuf, 0, pos);
                 if (!bufUpdater.compareAndSet(this, buffer, nbuf)) {
-                    // Can't replace buf if there was an async close.
-                    // Note: This would need to be changed if fill()
-                    // is ever made accessible to multiple threads.
-                    // But for now, the only way CAS can fail is via close.
-                    // assert buf == null;
+                    // 如果存在异步关闭, 则无法替换 buf.
+                    // 注意: 如果多个线程可以访问 fill(), 则需要更改此值.
+                    // 但是现在, CAS 失败的唯一方法是通过关闭.
+                    // 断言 buf == null;
                     throw new IOException("Stream closed");
                 }
                 buffer = nbuf;
             }
         }
+        // count 等于当前的位置
         count = pos;
+        // 填充整个缓冲区
         int n = getInIfOpen().read(buffer, pos, buffer.length - pos);
         if (n > 0) {
             count = n + pos;
@@ -216,48 +198,48 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * See
-     * the general contract of the <code>read</code>
-     * method of <code>InputStream</code>.
+     * 参阅 <code>InputStream</code> 的 <code>read</code> 方法.
      *
-     * @return the next byte of data, or <code>-1</code> if the end of the
-     * stream is reached.
-     * @throws IOException if this input stream has been closed by
-     *                     invoking its {@link #close()} method,
-     *                     or an I/O error occurs.
+     * @return 数据的下一个字节, 或者到达流的末尾时返回 <code>-1</code>.
+     * @throws IOException 如果流通过调用 {@link #close()} 方法被关闭, 或者出现 I/O 错误.
      * @see java.io.FilterInputStream#in
      */
     @Override
     public synchronized int read() throws IOException {
         if (pos >= count) {
+            // 不够用了? 填充缓冲区
             fill();
+            // 还不够? 可能没东西了
             if (pos >= count) {
                 return -1;
             }
         }
+        // 把 pos 位置的字节取低 8 位 转换为 int 类型的值
         return getBufIfOpen()[pos++] & 0xff;
     }
 
     /**
-     * Read characters into a portion of an array, reading from the underlying
-     * stream at most once if necessary.
+     * 将字符读入数组的一部分, 必要时最多从基础流中读取一次.
      */
     private int read1(byte[] b, int off, int len) throws IOException {
+        // 可以读取的长度
         int avail = count - pos;
         if (avail <= 0) {
-            /* If the requested length is at least as large as the buffer, and
-               if there is no mark/reset activity, do not bother to copy the
-               bytes into the local buffer.  In this way buffered streams will
-               cascade harmlessly. */
+            /* 如果请求的长度至少与缓冲区一样大, 并且没有 mark/reset 活动,
+               则不必费心将字节复制到本地缓冲区.
+               通过这种方式, 缓冲流将无害地级联.*/
             if (len >= getBufIfOpen().length && markpos < 0) {
                 return getInIfOpen().read(b, off, len);
             }
+            // len < getBufIfOpen().length || markpos >= 0
+            // 长度没超过缓冲区, 或者存在标记
             fill();
             avail = count - pos;
             if (avail <= 0) {
                 return -1;
             }
         }
+        // 长度和可读大小取最小值 cnt , 读取 cnt 长度的字节
         int cnt = (avail < len) ? avail : len;
         System.arraycopy(getBufIfOpen(), pos, b, off, cnt);
         pos += cnt;
@@ -265,46 +247,36 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * Reads bytes from this byte-input stream into the specified byte array,
-     * starting at the given offset.
+     * 从给定的偏移量开始, 将字节输入流中的字节读入指定的字节数组.
      *
-     * <p> This method implements the general contract of the corresponding
-     * <code>{@link InputStream#read(byte[], int, int) read}</code> method of
-     * the <code>{@link InputStream}</code> class.  As an additional
-     * convenience, it attempts to read as many bytes as possible by repeatedly
-     * invoking the <code>read</code> method of the underlying stream.  This
-     * iterated <code>read</code> continues until one of the following
-     * conditions becomes true: <ul>
+     * <p>该方法实现了 <code>{@link InputStream}</code> 类
+     * <code>{@link InputStream#read(byte[], int, int) read}</code>
+     * 方法的一般契约. 作为额外的便利, 它试图通过反复调用底层流的 <code>read</code>
+     * 方法来读取尽可能多的字节. 此迭代 <code>read</code> 直到满足以下条件之一为止: <ul>
      *
-     * <li> The specified number of bytes have been read,
+     * <li> 已读取指定的字节数,
      *
-     * <li> The <code>read</code> method of the underlying stream returns
-     * <code>-1</code>, indicating end-of-file, or
+     * <li> 基础流的 <code>read</code> 方法返回 <code>-1</code>,
+     * 表示文件结束, 或者
      *
-     * <li> The <code>available</code> method of the underlying stream
-     * returns zero, indicating that further input requests would block.
+     * <li> 基础流的 <code>available</code> 方法返回零, 表示进一步的输入请求被阻止.
      *
-     * </ul> If the first <code>read</code> on the underlying stream returns
-     * <code>-1</code> to indicate end-of-file then this method returns
-     * <code>-1</code>.  Otherwise this method returns the number of bytes
-     * actually read.
+     * </ul> 基础流的 <code>available</code> 方法如果基础流上的第一个
+     * <code>read</code> 返回 <code>-1</code>以指示文件结束, 则此方法返回 <code>-1</code>.
+     * 否则, 此方法返回实际读取的字节数. 返回零, 表示进一步的输入请求将被阻止.
      *
-     * <p> Subclasses of this class are encouraged, but not required, to
-     * attempt to read as many bytes as possible in the same fashion.
+     * <p> 鼓励但不要就此类的子类尝试以相同的方式读取尽可能多的字节.
      *
-     * @param b   destination buffer.
-     * @param off offset at which to start storing bytes.
-     * @param len maximum number of bytes to read.
-     * @return the number of bytes read, or <code>-1</code> if the end of
-     * the stream has been reached.
-     * @throws IOException if this input stream has been closed by
-     *                     invoking its {@link #close()} method,
-     *                     or an I/O error occurs.
+     * @param b   目标缓冲区.
+     * @param off 开始存储字节的偏移量.
+     * @param len 读取字节的最大数量.
+     * @return 读取的字节数, 如果已到达流的末尾, 则为 <code>-1</code>.
+     * @throws IOException 如果流通过调用 {@link #close()} 方法被关闭, 或者出现 I/O 错误.
      */
     @Override
     public synchronized int read(byte b[], int off, int len)
             throws IOException {
-        getBufIfOpen(); // Check for closed stream
+        getBufIfOpen(); // 检查是否关闭流
         if ((off | len | (off + len) | (b.length - (off + len))) < 0) {
             throw new IndexOutOfBoundsException();
         } else if (len == 0) {
@@ -313,15 +285,16 @@ class BufferedInputStream extends FilterInputStream {
 
         int n = 0;
         for (; ; ) {
-            int nread = read1(b, off + n, len - n);
+            int nread = read1(b, off + n, len - n);// nread 已读取的字节数
             if (nread <= 0) {
+                // 读取到达末尾
                 return (n == 0) ? nread : n;
             }
-            n += nread;
+            n += nread; // 统计字节数
             if (n >= len) {
                 return n;
             }
-            // if not closed but no bytes available, return
+            // 如果没有关闭但没有可用的字节, 则返回
             InputStream input = in;
             if (input != null && input.available() <= 0) {
                 return n;
@@ -330,29 +303,26 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * See the general contract of the <code>skip</code>
-     * method of <code>InputStream</code>.
+     * 请参阅 <code>InputStream</code> 类的 <code>skip</code> 方法的一般合约.
      *
-     * @throws IOException if the stream does not support seek,
-     *                     or if this input stream has been closed by
-     *                     invoking its {@link #close()} method, or an
-     *                     I/O error occurs.
+     * @throws IOException 如果流不支持 seek, 或者通过调用其 {@link #close()} 方法
+     *                     关闭此输入流, 或者发生 I/O 错误.
      */
     @Override
     public synchronized long skip(long n) throws IOException {
-        getBufIfOpen(); // Check for closed stream
+        getBufIfOpen(); //检查是否是关闭的流
         if (n <= 0) {
             return 0;
         }
         long avail = count - pos;
 
         if (avail <= 0) {
-            // If no mark position set then don't keep in buffer
+            // 如果没有设置标记位置, 则不要保留缓冲区
             if (markpos < 0) {
                 return getInIfOpen().skip(n);
             }
 
-            // Fill in buffer to save bytes for reset
+            // 填写缓冲区以保存字节以进行重置
             fill();
             avail = count - pos;
             if (avail <= 0) {
@@ -366,21 +336,14 @@ class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * Returns an estimate of the number of bytes that can be read (or
-     * skipped over) from this input stream without blocking by the next
-     * invocation of a method for this input stream. The next invocation might be
-     * the same thread or another thread.  A single read or skip of this
-     * many bytes will not block, but may read or skip fewer bytes.
+     * 返回可以从此输入流中读取(或跳过)的字节数的估计值, 而不会被下一次调用此输入流的方法阻塞.
+     * 下一次调用可能是同一个线程或另一个线程. 单个读取或跳过这么多字节不会阻塞, 但可以读取或跳过更少的字节.
      * <p>
-     * This method returns the sum of the number of bytes remaining to be read in
-     * the buffer (<code>count&nbsp;- pos</code>) and the result of calling the
-     * {@link java.io.FilterInputStream#in in}.available().
+     * 此方法返回缓冲区中剩余要读取的字节数 (<code>count&nbsp;- pos</code>) 和
+     * 调用 {@link java.io.FilterInputStream#in in}.available() 的结果之和.
      *
-     * @return an estimate of the number of bytes that can be read (or skipped
-     * over) from this input stream without blocking.
-     * @throws IOException if this input stream has been closed by
-     *                     invoking its {@link #close()} method,
-     *                     or an I/O error occurs.
+     * @return 估计可以从此输入流中无阻塞地读取(或跳过)的字节数.
+     * @throws IOException 如果输入流通过调用其 {@link #close()} 方法关闭此输入流, 或者发生 I/O 错误.
      */
     @Override
     public synchronized int available() throws IOException {
