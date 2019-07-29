a8c1b05d24f645f9652846daccadf9fadf09e5ba
Wttch[Home-Win10] <admin@wttch.com>
2019-07-29 23:12:01 +0800

    java.lang.Readable;

--- src/main/java/java/lang/Readable.java
+++ src/main/java/java/lang/Readable.java
@@ -1,50 +1,22 @@
-/*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 import java.io.IOException;
 
 /**
- * A <tt>Readable</tt> is a source of characters. Characters from
- * a <tt>Readable</tt> are made available to callers of the read
- * method via a {@link java.nio.CharBuffer CharBuffer}.
+ * <tt>Readable</tt> 是一个字符来源. 来自 <tt>Readable</tt> 的字符通过
+ * {@link java.nio.CharBuffer} 提供的 read 方法的调用者.
  *
  * @since 1.5
  */
 public interface Readable {
 
     /**
-     * Attempts to read characters into the specified character buffer.
-     * The buffer is used as a repository of characters as-is: the only
-     * changes made are the results of a put operation. No flipping or
-     * rewinding of the buffer is performed.
+     * 试图读取字符放入指定的字符缓冲区. 缓冲区按原样用作字符存储库:
+     * 唯一变化是 put 操作的结果. 不执行缓冲器的翻转或者逆序.
      *
-     * @param cb the buffer to read characters into
-     * @return The number of {@code char} values added to the buffer,
-     * or -1 if this source of characters is at its end
+     * @param cb 用于读取字符的缓冲区
+     * @return 添加到缓冲区的 {@code char} 值的数量,
+     * 如果此字符源位于其末尾, 则为 -1.
      * @throws IOException                      if an I/O error occurs
      * @throws NullPointerException             if cb is null
      * @throws java.nio.ReadOnlyBufferException if cb is a read only buffer
