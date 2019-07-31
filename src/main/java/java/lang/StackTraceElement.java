a17b30bb1c1142ea20e04832c872b63436117069
Wttch <admin@wttch.com>
2019-07-31 18:49:14 +0800

    java.lang.StackTraceElement;

--- src/main/java/java/lang/StackTraceElement.java
+++ src/main/java/java/lang/StackTraceElement.java
@@ -1,69 +1,31 @@
-/*
- * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 import java.util.Objects;
 
 /**
- * An element in a stack trace, as returned by {@link
- * Throwable#getStackTrace()}.  Each element represents a single stack frame.
- * All stack frames except for the one at the top of the stack represent
- * a method invocation.  The frame at the top of the stack represents the
- * execution point at which the stack trace was generated.  Typically,
- * this is the point at which the throwable corresponding to the stack trace
- * was created.
+ * 堆栈跟踪中的一个元素, 由 {@link Throwable#getStackTrace()} 返回.
+ * 每个元素代表一个堆栈帧. 除堆栈顶部的堆栈帧之外的所有堆栈帧表示方法调用.
+ * 通常, 这是创建与堆栈跟踪相对应的 Throwable 的点.
  *
  * @author Josh Bloch
  * @since 1.4
  */
 public final class StackTraceElement implements java.io.Serializable {
-    // Normally initialized by VM (public constructor added in 1.5)
+    // 通常由VM初始化(1.5中添加的公共构造函数)
     private String declaringClass;
     private String methodName;
     private String fileName;
     private int lineNumber;
 
     /**
-     * Creates a stack trace element representing the specified execution
-     * point.
+     * 创建表示指定执行点的堆栈跟踪元素.
      *
-     * @param declaringClass the fully qualified name of the class containing
-     *                       the execution point represented by the stack trace element
-     * @param methodName     the name of the method containing the execution point
-     *                       represented by the stack trace element
-     * @param fileName       the name of the file containing the execution point
-     *                       represented by the stack trace element, or {@code null} if
-     *                       this information is unavailable
-     * @param lineNumber     the line number of the source line containing the
-     *                       execution point represented by this stack trace element, or
-     *                       a negative number if this information is unavailable. A value
-     *                       of -2 indicates that the method containing the execution point
-     *                       is a native method
-     * @throws NullPointerException if {@code declaringClass} or
-     *                              {@code methodName} is null
+     * @param declaringClass 包含由堆栈跟踪元素表示的执行点的类的完全限定名称
+     * @param methodName     包含由堆栈跟踪元素表示的执行点的方法的名称
+     * @param fileName       包含由堆栈跟踪元素表示的执行点的文件的名称, 如果此信息不可用, 则为 {@code null}
+     * @param lineNumber     包含由堆栈跟踪元素表示的执行点的源行的行号, 如果此信息不可用, 则为负数.
+     *                       -2 表示包含执行点的方法是本机方法
+     * @throws NullPointerException 如果 {@code declaringClass} 或者 {@code methodName} 为 null
      * @since 1.5
      */
     public StackTraceElement(String declaringClass, String methodName,
@@ -75,101 +37,80 @@ public final class StackTraceElement implements java.io.Serializable {
     }
 
     /**
-     * Returns the name of the source file containing the execution point
-     * represented by this stack trace element.  Generally, this corresponds
-     * to the {@code SourceFile} attribute of the relevant {@code class}
-     * file (as per <i>The Java Virtual Machine Specification</i>, Section
-     * 4.7.7).  In some systems, the name may refer to some source code unit
-     * other than a file, such as an entry in source repository.
+     * 返回包含此堆栈跟踪元素表示的执行点的源文件的名称. 通常, 这对应于相关 {@code class} 文件的
+     * {@code SourceFile}属性(根据<i>Java虚拟机规范</i>, 第4.7.7节). 在某些系统中,
+     * 名称可以指代除文件之外的某些源代码单元, 例如源存储库中的条目.
      *
-     * @return the name of the file containing the execution point
-     * represented by this stack trace element, or {@code null} if
-     * this information is unavailable.
+     * @return 包含此堆栈跟踪元素表示的执行点的文件的名称, 如果此信息不可用, 则为 {@code null}.
      */
     public String getFileName() {
         return fileName;
     }
 
     /**
-     * Returns the line number of the source line containing the execution
-     * point represented by this stack trace element.  Generally, this is
-     * derived from the {@code LineNumberTable} attribute of the relevant
-     * {@code class} file (as per <i>The Java Virtual Machine
-     * Specification</i>, Section 4.7.8).
+     * 返回包含此堆栈跟踪元素表示的执行点的源代码行号. 通常, 这是从相关 {@code class} 文件的
+     * {@code LineNumberTable} 属性派生的(根据<i>Java虚拟机规范</i>, 第4.7.8节).
      *
-     * @return the line number of the source line containing the execution
-     * point represented by this stack trace element, or a negative
-     * number if this information is unavailable.
+     * @return 包含此堆栈跟踪元素表示的执行点的源行的行号, 如果此信息不可用, 则为负数.
      */
     public int getLineNumber() {
         return lineNumber;
     }
 
     /**
-     * Returns the fully qualified name of the class containing the
-     * execution point represented by this stack trace element.
+     * 返回包含此堆栈跟踪元素表示的执行点的类的完全限定名称.
      *
-     * @return the fully qualified name of the {@code Class} containing
-     * the execution point represented by this stack trace element.
+     * @return 包含此堆栈跟踪元素表示的执行点的 {@code Class} 的完全限定名称.
      */
     public String getClassName() {
         return declaringClass;
     }
 
     /**
-     * Returns the name of the method containing the execution point
-     * represented by this stack trace element.  If the execution point is
-     * contained in an instance or class initializer, this method will return
-     * the appropriate <i>special method name</i>, {@code <init>} or
-     * {@code <clinit>}, as per Section 3.9 of <i>The Java Virtual
-     * Machine Specification</i>.
+     * 返回包含此堆栈跟踪元素表示的执行点的方法的名称. 如果执行点包含在实例或类初始化程序中,
+     * 则此方法将返回相应的 <i>特殊方法名称</i>, {@code <init>} 或 {@code <clinit>},
+     * 如第3.9节所述 <i>Java虚拟机规范</i>.
      *
-     * @return the name of the method containing the execution point
-     * represented by this stack trace element.
+     * @return 包含此堆栈跟踪元素表示的执行点的方法的名称.
      */
     public String getMethodName() {
         return methodName;
     }
 
     /**
-     * Returns true if the method containing the execution point
-     * represented by this stack trace element is a native method.
+     * 如果包含此堆栈跟踪元素表示的执行点的方法是本机方法, 则返回true.
      *
-     * @return {@code true} if the method containing the execution point
-     * represented by this stack trace element is a native method.
+     * @return {@code true} 如果包含此堆栈跟踪元素表示的执行点的方法是本机方法.
      */
     public boolean isNativeMethod() {
         return lineNumber == -2;
     }
 
     /**
-     * Returns a string representation of this stack trace element.  The
-     * format of this string depends on the implementation, but the following
-     * examples may be regarded as typical:
+     * 返回此堆栈跟踪元素的字符串表示形式.
+     * 此字符串的格式取决于实现, 但以下示例可能被视为典型:
      * <ul>
      * <li>
-     * {@code "MyClass.mash(MyClass.java:9)"} - Here, {@code "MyClass"}
-     * is the <i>fully-qualified name</i> of the class containing the
-     * execution point represented by this stack trace element,
-     * {@code "mash"} is the name of the method containing the execution
-     * point, {@code "MyClass.java"} is the source file containing the
-     * execution point, and {@code "9"} is the line number of the source
-     * line containing the execution point.
+     * {@code "MyClass.mash(MyClass.java:9)"} - 这里,
+     * {@code "MyClass"} 是包含此堆栈跟踪元素表示的执行点的类的<i>完全限定名称</i>,
+     * {@code "mash"} 是包含该方法执行点的名称,
+     * {@code "MyClass.java"} 是包含执行点的源文件,
+     * {@code "9"} 是包含执行点的源代码行号.
      * <li>
-     * {@code "MyClass.mash(MyClass.java)"} - As above, but the line
-     * number is unavailable.
+     * {@code "MyClass.mash(MyClass.java)"} - 如上所述, 但行号不可用.
      * <li>
-     * {@code "MyClass.mash(Unknown Source)"} - As above, but neither
-     * the file name nor the line  number are available.
+     * {@code "MyClass.mash(Unknown Source)"} - 如上所述, 但文件名和行号都不可用.
      * <li>
-     * {@code "MyClass.mash(Native Method)"} - As above, but neither
-     * the file name nor the line  number are available, and the method
-     * containing the execution point is known to be a native method.
+     * {@code "MyClass.mash(Native Method)"} - 如上所述, 但文件名和行号都不可用, 并且包含执行点的方法已知是本机方法.
      * </ul>
      *
      * @see Throwable#printStackTrace()
      */
     public String toString() {
+        // 本地方法: (Native Method)
+        //     fileName 存在并且 lineNumber >= 0: (fileName:lineNumber)
+        //         fileName 存在: (fileName)
+        //             (Unknown Source)
         return getClassName() + "." + methodName +
                 (isNativeMethod() ? "(Native Method)" :
                         (fileName != null && lineNumber >= 0 ?
@@ -178,23 +119,19 @@ public final class StackTraceElement implements java.io.Serializable {
     }
 
     /**
-     * Returns true if the specified object is another
-     * {@code StackTraceElement} instance representing the same execution
-     * point as this instance.  Two stack trace elements {@code a} and
-     * {@code b} are equal if and only if:
+     * 如果指定的对象是另一个表示与此实例相同的执行点的 {@code StackTraceElement} 实例, 则返回 true.
+     * 当且仅当以下情况时, 两个堆栈跟踪元素 {@code a} 和 {@code b} 是相等的:
      * <pre>{@code
      *     equals(a.getFileName(), b.getFileName()) &&
      *     a.getLineNumber() == b.getLineNumber()) &&
      *     equals(a.getClassName(), b.getClassName()) &&
      *     equals(a.getMethodName(), b.getMethodName())
      * }</pre>
-     * where {@code equals} has the semantics of {@link
-     * java.util.Objects#equals(Object, Object) Objects.equals}.
+     * 其中 {@code equals} 的语义为 {@link java.util.Objects#equals(Object, Object)}.
      *
-     * @param obj the object to be compared with this stack trace element.
-     * @return true if the specified object is another
-     * {@code StackTraceElement} instance representing the same
-     * execution point as this instance.
+     * @param obj 要与此堆栈跟踪元素进行比较的对象.
+     * @return 如果指定的对象是另一个表示与此实例相同的执行点的 {@code StackTraceElement} 实例,
+     * 则返回true.
      */
     public boolean equals(Object obj) {
         if (obj == this)
@@ -209,7 +146,11 @@ public final class StackTraceElement implements java.io.Serializable {
     }
 
     /**
-     * Returns a hash code value for this stack trace element.
+     * 返回此堆栈跟踪元素的哈希值.
+     * <p>
+     * wttch: 使用 31 作为乘子的原因:
+     * 1. 优选的质数, 也可以 37/41/43 等较小的质数, 可以预防一些 hash 冲突;
+     * 2. 31 可以被 JVM 调优, 31 * i = (i << 5) - i;
      */
     public int hashCode() {
         int result = 31 * declaringClass.hashCode() + methodName.hashCode();
