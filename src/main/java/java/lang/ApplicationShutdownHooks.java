5ce18664338015da1a4d7be31a75123eba6a9f70
Wttch <admin@wttch.com>
2019-08-05 18:54:49 +0800

    java.lang.ApplicationShutdownHooks;

--- src/main/java/java/lang/ApplicationShutdownHooks.java
+++ src/main/java/java/lang/ApplicationShutdownHooks.java
@@ -1,57 +1,27 @@
-/*
- * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package java.lang;
 
 import java.util.*;
 
-/*
- * Class to track and run user level shutdown hooks registered through
- * <tt>{@link Runtime#addShutdownHook Runtime.addShutdownHook}</tt>.
+/**
+ * 用于跟踪和运行通过 <tt>{@link Runtime#addShutdownHook(Thread)
+ * Runtime.addShutdownHook}</tt> 注册的用户级别关闭钩子的类.
  *
- * @see java.lang.Runtime#addShutdownHook
- * @see java.lang.Runtime#removeShutdownHook
+ * @see java.lang.Runtime#addShutdownHook(Thread)
+ * @see java.lang.Runtime#removeShutdownHook(Thread)
  */
-
 class ApplicationShutdownHooks {
-    /* The set of registered hooks */
+    /* 注册的钩子集合 */
     private static IdentityHashMap<Thread, Thread> hooks;
 
     static {
         try {
-            Shutdown.add(1 /* shutdown hook invocation order */,
-                    false /* not registered if shutdown in progress */,
-                    new Runnable() {
-                        public void run() {
-                            runHooks();
-                        }
-                    }
+            Shutdown.add(1 /* shutdown hook 调用顺序 */,
+                    false /* 如果进程正在关闭, 则不注册 */,
+                    ApplicationShutdownHooks::runHooks
             );
             hooks = new IdentityHashMap<>();
         } catch (IllegalStateException e) {
-            // application shutdown hooks cannot be added if
-            // shutdown is in progress.
+            // 当进程正在关闭的时候, 程序关闭钩子不能被添加
             hooks = null;
         }
     }
@@ -60,38 +30,43 @@ class ApplicationShutdownHooks {
     private ApplicationShutdownHooks() {
     }
 
-    /* Add a new shutdown hook.  Checks the shutdown state and the hook itself,
-     * but does not do any security checks.
+    /*
+     * 添加一个新的关闭钩子. 检查关闭状态和钩子自身, 但是不做任何安全检查.
      */
     static synchronized void add(Thread hook) {
         if (hooks == null)
+            // 进程正在关闭
             throw new IllegalStateException("Shutdown in progress");
 
         if (hook.isAlive())
+            // 钩子已经被添加, 并且正在运行
             throw new IllegalArgumentException("Hook already running");
 
         if (hooks.containsKey(hook))
+            // 钩子已经存在了
             throw new IllegalArgumentException("Hook previously registered");
 
         hooks.put(hook, hook);
     }
 
-    /* Remove a previously-registered hook.  Like the add method, this method
-     * does not do any security checks.
+    /* Remove a previously-registered hook. 像 add 方法一样, 它也不做安全检查.
+     * 移除之前注册的钩子.
      */
     static synchronized boolean remove(Thread hook) {
         if (hooks == null)
+            // 正在关闭
             throw new IllegalStateException("Shutdown in progress");
 
         if (hook == null)
+            // 空钩子
             throw new NullPointerException();
 
         return hooks.remove(hook) != null;
     }
 
-    /* Iterates over all application hooks creating a new thread for each
-     * to run in. Hooks are run concurrently and this method waits for
-     * them to finish.
+    /*
+     * 迭代所有的程序钩子, 为它们创建一个线程来运行它们.
+     * 钩子们并行运行, 并且这个方法等待它们运行结束.
      */
     static void runHooks() {
         Collection<Thread> threads;
