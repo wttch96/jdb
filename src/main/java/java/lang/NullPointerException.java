b65ad6d746e4408f0375843965658e5326c5523b
Wttch <admin@wttch.com>
2019-08-05 17:42:26 +0800

    java.lang.NullPointerException;

--- src/main/java/java/lang/NullPointerException.java
+++ src/main/java/java/lang/NullPointerException.java
@@ -1,50 +1,20 @@
-/*
- * Copyright (c) 1994, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * Thrown when an application attempts to use {@code null} in a
- * case where an object is required. These include:
+ * 当程序试图在需要 Object 的时候使用 {@code null} 时抛出. 它们包括:
  * <ul>
- * <li>Calling the instance method of a {@code null} object.
- * <li>Accessing or modifying the field of a {@code null} object.
- * <li>Taking the length of {@code null} as if it were an array.
- * <li>Accessing or modifying the slots of {@code null} as if it
- * were an array.
- * <li>Throwing {@code null} as if it were a {@code Throwable}
- * value.
+ * <li>调用 {@code null} 对象的实例方法时.
+ * <li>访问或者修改 {@code null} 对象的字段时.
+ * <li>将 {@code null} 的长度视为数组.
+ * <li>访问或修改 {@code null} 的插槽, 就像它是一个数组一样.
+ * <li>抛出 {@code null},  好像它是 {@code Throwable} 值.
  * </ul>
  * <p>
- * Applications should throw instances of this class to indicate
- * other illegal uses of the {@code null} object.
+ * 应用程序应抛出此类的实例以指示 {@code null} 对象的其他非法使用.
  * <p>
- * {@code NullPointerException} objects may be constructed by the
- * virtual machine as if {@linkplain Throwable#Throwable(String,
- * Throwable, boolean, boolean) suppression were disabled and/or the
- * stack trace was not writable}.
+ * {@code NullPointerException} 对象可有虚拟机构造, 就好像
+ * {@linkplain Throwable#Throwable(String, Throwable, boolean, boolean)
+ * 抑制被禁用和/或堆栈跟踪不可写}.
  *
  * @author unascribed
  * @since JDK1.0
@@ -54,17 +24,16 @@ class NullPointerException extends RuntimeException {
     private static final long serialVersionUID = 5162710183389028792L;
 
     /**
-     * Constructs a {@code NullPointerException} with no detail message.
+     * 构造 {@code NullPointerException} 不使用详细消息.
      */
     public NullPointerException() {
         super();
     }
 
     /**
-     * Constructs a {@code NullPointerException} with the specified
-     * detail message.
+     * 构造 {@code NullPointerException} 使用指定的详细消息.
      *
-     * @param s the detail message.
+     * @param s 详细消息.
      */
     public NullPointerException(String s) {
         super(s);
