01146cca7bbd43ce8152b67a0618527e4740faa7
Wttch[Home-Win10] <admin@wttch.com>
2019-08-06 01:55:20 +0800

    java.lang.ArithmeticException;

--- src/main/java/java/lang/ArithmeticException.java
+++ src/main/java/java/lang/ArithmeticException.java
@@ -1,39 +1,11 @@
-/*
- * Copyright (c) 1994, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * Thrown when an exceptional arithmetic condition has occurred. For
- * example, an integer "divide by zero" throws an
- * instance of this class.
+ * 发生异常算术条件时抛出. 例如, 一个整型"除零"抛出这个类的实例.
  * <p>
- * {@code ArithmeticException} objects may be constructed by the
- * virtual machine as if {@linkplain Throwable#Throwable(String,
- * Throwable, boolean, boolean) suppression were disabled and/or the
- * stack trace was not writable}.
+ * {@code ArithmeticException} 对象可以由虚拟机构造, 就好像
+ * {@linkplain Throwable#Throwable(String, Throwable, boolean, boolean)
+ * 抑制被禁用和/或堆栈跟踪不可写}.
  *
  * @author unascribed
  * @since JDK1.0
@@ -42,18 +14,16 @@ public class ArithmeticException extends RuntimeException {
     private static final long serialVersionUID = 2256477558314496007L;
 
     /**
-     * Constructs an {@code ArithmeticException} with no detail
-     * message.
+     * 构造 {@code ArithmeticException} 不使用详细信息.
      */
     public ArithmeticException() {
         super();
     }
 
     /**
-     * Constructs an {@code ArithmeticException} with the specified
-     * detail message.
+     * 构造 {@code ArithmeticException} 使用制定的详细信息.
      *
-     * @param s the detail message.
+     * @param s 详细信息.
      */
     public ArithmeticException(String s) {
         super(s);
