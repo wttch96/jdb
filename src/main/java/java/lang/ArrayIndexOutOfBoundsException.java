d38b80dabb0c9a5b295e487fe4e2cb8347bf9db7
Wttch <admin@wttch.com>
2019-08-06 18:35:32 +0800

    java.lang.ArrayIndexOutOfBoundsException;

--- src/main/java/java/lang/ArrayIndexOutOfBoundsException.java
+++ src/main/java/java/lang/ArrayIndexOutOfBoundsException.java
@@ -1,34 +1,7 @@
-/*
- * Copyright (c) 1994, 2008, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * Thrown to indicate that an array has been accessed with an
- * illegal index. The index is either negative or greater than or
- * equal to the size of the array.
+ * 抛出以指示已使用非法索引访问数组. 索引为负数或大于等于数组的大小.
  *
  * @author unascribed
  * @since JDK1.0
@@ -38,16 +11,14 @@ class ArrayIndexOutOfBoundsException extends IndexOutOfBoundsException {
     private static final long serialVersionUID = -5116101128118950844L;
 
     /**
-     * Constructs an <code>ArrayIndexOutOfBoundsException</code> with no
-     * detail message.
+     * 不使用详细信息构造 <code>ArrayIndexOutOfBoundsException</code> .
      */
     public ArrayIndexOutOfBoundsException() {
         super();
     }
 
     /**
-     * Constructs a new <code>ArrayIndexOutOfBoundsException</code>
-     * class with an argument indicating the illegal index.
+     * 用一个表明非法索引的参数构造 <code>ArrayIndexOutOfBoundsException</code>.
      *
      * @param index the illegal index.
      */
@@ -56,10 +27,9 @@ class ArrayIndexOutOfBoundsException extends IndexOutOfBoundsException {
     }
 
     /**
-     * Constructs an <code>ArrayIndexOutOfBoundsException</code> class
-     * with the specified detail message.
+     * 使用指定的详细消息构造 <code>ArrayIndexOutOfBoundsException</code> .
      *
-     * @param s the detail message.
+     * @param s 详细消息.
      */
     public ArrayIndexOutOfBoundsException(String s) {
         super(s);
