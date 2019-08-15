24c768527312b4578926f29edde4de76d21b5d8d
Wttch[Home-Win10] <admin@wttch.com>
2019-08-15 23:18:21 +0800

    java.lang.Error;

--- src/main/java/java/lang/Error.java
+++ src/main/java/java/lang/Error.java
@@ -1,48 +1,17 @@
-/*
- * Copyright (c) 1995, 2011, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * An {@code Error} is a subclass of {@code Throwable}
- * that indicates serious problems that a reasonable application
- * should not try to catch. Most such errors are abnormal conditions.
- * The {@code ThreadDeath} error, though a "normal" condition,
- * is also a subclass of {@code Error} because most applications
- * should not try to catch it.
+ * {@code Error} 是表示一个合理的程序发生了严重错误并且不应该尝试捕获的 {@code Throwable} 子类.
+ * 大多数错误是异常条件. {@code ThreadDeath} 错误, 虽然是"正常"条件, 但是也是 {@code Error} 的子类,
+ * 因为大多数应用程序都不应该尝试捕获它.
  * <p>
- * A method is not required to declare in its {@code throws}
- * clause any subclasses of {@code Error} that might be thrown
- * during the execution of the method but not caught, since these
- * errors are abnormal conditions that should never occur.
+ * 一个方法不需要在其 {@code throws} 子句中声明在执行期间可能抛出但未捕获的 {@code Error} 和
+ * 它的任何子类, 因为这些错误是永远不应发生的异常情况.
  * <p>
- * That is, {@code Error} and its subclasses are regarded as unchecked
- * exceptions for the purposes of compile-time checking of exceptions.
+ * 这就是, {@code Error} 和它的子类被视为不受检异常, 用于编译时异常检查.
  *
  * @author Frank Yellin
- * @jls 11.2 Compile-Time Checking of Exceptions
+ * @jls 11.2 编译时异常检查
  * @see java.lang.ThreadDeath
  * @since JDK1.0
  */
@@ -50,38 +19,30 @@ public class Error extends Throwable {
     static final long serialVersionUID = 4980196508277280342L;
 
     /**
-     * Constructs a new error with {@code null} as its detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
+     * 使用 {@code null} 作为详细信息构造新的 Error. 原因未被初始化, 稍候可以通过调用
+     * {@link #initCause(Throwable)} 来初始化原因.
      */
     public Error() {
         super();
     }
 
     /**
-     * Constructs a new error with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause}.
+     * 使用指定的信息构造新的 Error, 原因未被初始化, 稍候可以通过调用 {@link #initCause(Throwable)}
+     * 方法来初始化原因.
      *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
+     * @param message 详细消息. 详细消息被保存, 稍候可以通过 {@link #getMessage()} 方法恢复.
      */
     public Error(String message) {
         super(message);
     }
 
     /**
-     * Constructs a new error with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * {@code cause} is <i>not</i> automatically incorporated in
-     * this error's detail message.
+     * 使用指定的信息和原因构造新的 Error. <p>注意 {@code cause} 相关联的详细信息 <em>不会</em>
+     * 自动合并到这个 Error 的详细信息中去.
      *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A {@code null} value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
+     * @param message 详细消息. 详细消息被保存, 稍候可以通过 {@link #getMessage()} 方法恢复.
+     * @param cause   原因 (将被保存, 稍候可以通过 {@link #getCause()} 方法恢复).
+     *                (允许 {@code null} 值, 表示原因不存在或未知.)
      * @since 1.4
      */
     public Error(String message, Throwable cause) {
@@ -89,34 +50,30 @@ public class Error extends Throwable {
     }
 
     /**
-     * Constructs a new error with the specified cause and a detail
-     * message of {@code (cause==null ? null : cause.toString())} (which
-     * typically contains the class and detail message of {@code cause}).
-     * This constructor is useful for errors that are little more than
-     * wrappers for other throwables.
+     * 构造一个新的 Error 使用指定的原因和详细消息
+     * ({@code (cause==null ? null : cause.toString())}
+     * (通常包含 {@code cause} 的类和详细消息)).
+     * 这个构造函数对 throwables 包装其他 throwables 是有用的.
+     * (例如, {@link java.security.PrivilegedActionException}).
      *
-     * @param cause the cause (which is saved for later retrieval by the
-     *              {@link #getCause()} method).  (A {@code null} value is
-     *              permitted, and indicates that the cause is nonexistent or
-     *              unknown.)
+     * <p>调用 {@link #fillInStackTrace()} 方法来初始化新创建的 Throwable 中的堆栈跟踪数据.
+     *
+     * @param cause 原因 (将被保存, 稍候可以通过 {@link #getCause()} 方法恢复).
+     *              (允许 {@code null} 值, 表示原因不存在或未知.)
      * @since 1.4
      */
     public Error(Throwable cause) {
         super(cause);
     }
 
+
     /**
-     * Constructs a new error with the specified detail message,
-     * cause, suppression enabled or disabled, and writable stack
-     * trace enabled or disabled.
+     * 使用指定的详细信息, 原因, 是否启用抑制，堆栈跟踪是否开启来构造新的 Error.
      *
-     * @param message            the detail message.
-     * @param cause              the cause.  (A {@code null} value is permitted,
-     *                           and indicates that the cause is nonexistent or unknown.)
-     * @param enableSuppression  whether or not suppression is enabled
-     *                           or disabled
-     * @param writableStackTrace whether or not the stack trace should
-     *                           be writable
+     * @param message            详细信息.
+     * @param cause              原因.  (允许 {@code null} 值, 表示原因不存在或者未知.)
+     * @param enableSuppression  是否启用异常抑制
+     * @param writableStackTrace 堆栈跟踪是否可写
      * @since 1.7
      */
     protected Error(String message, Throwable cause,
