8c614686ea5c03783cdc1e2f0fb6844de182d091
Wttch[Home-Win10] <admin@wttch.com>
2019-08-18 20:04:10 +0800

    java.lang.FunctionalInterface;

--- src/main/java/java/lang/FunctionalInterface.java
+++ src/main/java/java/lang/FunctionalInterface.java
@@ -1,61 +1,26 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 import java.lang.annotation.*;
 
 /**
- * An informative annotation type used to indicate that an interface
- * type declaration is intended to be a <i>functional interface</i> as
- * defined by the Java Language Specification.
+ * 一个信息性的注解类型, 用于指示接口类型声明旨在成为 java 语言规范定义的 <i>功能接口</i>.
  * <p>
- * Conceptually, a functional interface has exactly one abstract
- * method.  Since {@linkplain java.lang.reflect.Method#isDefault()
- * default methods} have an implementation, they are not abstract.  If
- * an interface declares an abstract method overriding one of the
- * public methods of {@code java.lang.Object}, that also does
- * <em>not</em> count toward the interface's abstract method count
- * since any implementation of the interface will have an
- * implementation from {@code java.lang.Object} or elsewhere.
+ * 从概念上讲, 功能接口只是一种抽象方法.
+ * 因为 {@linkplain java.lang.reflect.Method#isDefault() 默认方法} 有一个实现, 所以它们
+ * 不是抽象的. 如果一个接口判断一个抽象方法覆盖了 {@code java.lang.Object} 的一个公共
+ * 方法, 那么也不会计算接口的抽象方法的数量, 因为接口的任何实现都将具有来自 {@code
+ * java.lang.Object} 或其他地方的实现.
  *
- * <p>Note that instances of functional interfaces can be created with
- * lambda expressions, method references, or constructor references.
+ * <p>请注意, 可以使用 lambda 表达式, 方法引用或构造函数引用创建功能接口的实例.
  *
- * <p>If a type is annotated with this annotation type, compilers are
- * required to generate an error message unless:
+ * <p>如果使用此注解类型对类型进行标注, 则编译器会生成错误消息, 除非:
  *
  * <ul>
- * <li> The type is an interface type and not an annotation type, enum, or class.
- * <li> The annotated type satisfies the requirements of a functional interface.
+ * <li> 类型是接口类型, 而不是注解类型, 枚举或类.
+ * <li> 带注解的类型满足功能接口的要求.
  * </ul>
  *
- * <p>However, the compiler will treat any interface meeting the
- * definition of a functional interface as a functional interface
- * regardless of whether or not a {@code FunctionalInterface}
- * annotation is present on the interface declaration.
+ * <p>但是, 无论接口声明中是否存在 {@code FunctionInterface} 注解, 编译器都会将满足功能定义的任何接口视为功能接口.
  *
  * @jls 4.3.2. The Class Object
  * @jls 9.8 Functional Interfaces
