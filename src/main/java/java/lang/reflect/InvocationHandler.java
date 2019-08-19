e81071eb88810a3b0d1477f214079207fd20cc7f
Wttch <admin@wttch.com>
2019-08-19 18:34:55 +0800

    java.lang.reflect.InvocationHandler;

--- src/main/java/java/lang/reflect/InvocationHandler.java
+++ src/main/java/java/lang/reflect/InvocationHandler.java
@@ -1,38 +1,10 @@
-/*
- * Copyright (c) 1999, 2006, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * {@code InvocationHandler} is the interface implemented by
- * the <i>invocation handler</i> of a proxy instance.
+ * {@code InvocationHandler} 是由代理实例的<i>调用处理程序</i>实现的接口.
  *
- * <p>Each proxy instance has an associated invocation handler.
- * When a method is invoked on a proxy instance, the method
- * invocation is encoded and dispatched to the {@code invoke}
- * method of its invocation handler.
+ * <p>每个代理实例都有一个关联的调用处理程序. 在代理实例上调用方法时,
+ * 方法调用将被编码并调度到其处理程序的 {@code invoke} 方法.
  *
  * @author Peter Jones
  * @see Proxy
@@ -41,48 +13,23 @@ package java.lang.reflect;
 public interface InvocationHandler {
 
     /**
-     * Processes a method invocation on a proxy instance and returns
-     * the result.  This method will be invoked on an invocation handler
-     * when a method is invoked on a proxy instance that it is
-     * associated with.
+     * 处理代理实例上的方法调用并返回结果. 在与其关联的代理实例上调用方法时, 将在调用处理程序上调用此方法.
      *
-     * @param proxy  the proxy instance that the method was invoked on
-     * @param method the {@code Method} instance corresponding to
-     *               the interface method invoked on the proxy instance.  The declaring
-     *               class of the {@code Method} object will be the interface that
-     *               the method was declared in, which may be a superinterface of the
-     *               proxy interface that the proxy class inherits the method through.
-     * @param args   an array of objects containing the values of the
-     *               arguments passed in the method invocation on the proxy instance,
-     *               or {@code null} if interface method takes no arguments.
-     *               Arguments of primitive types are wrapped in instances of the
-     *               appropriate primitive wrapper class, such as
-     *               {@code java.lang.Integer} or {@code java.lang.Boolean}.
-     * @return the value to return from the method invocation on the
-     * proxy instance.  If the declared return type of the interface
-     * method is a primitive type, then the value returned by
-     * this method must be an instance of the corresponding primitive
-     * wrapper class; otherwise, it must be a type assignable to the
-     * declared return type.  If the value returned by this method is
-     * {@code null} and the interface method's return type is
-     * primitive, then a {@code NullPointerException} will be
-     * thrown by the method invocation on the proxy instance.  If the
-     * value returned by this method is otherwise not compatible with
-     * the interface method's declared return type as described above,
-     * a {@code ClassCastException} will be thrown by the method
-     * invocation on the proxy instance.
-     * @throws Throwable the exception to throw from the method
-     *                   invocation on the proxy instance.  The exception's type must be
-     *                   assignable either to any of the exception types declared in the
-     *                   {@code throws} clause of the interface method or to the
-     *                   unchecked exception types {@code java.lang.RuntimeException}
-     *                   or {@code java.lang.Error}.  If a checked exception is
-     *                   thrown by this method that is not assignable to any of the
-     *                   exception types declared in the {@code throws} clause of
-     *                   the interface method, then an
-     *                   {@link UndeclaredThrowableException} containing the
-     *                   exception that was thrown by this method will be thrown by the
-     *                   method invocation on the proxy instance.
+     * @param proxy  调用该方法的代理实例
+     * @param method 与在代理实例上调用的接口方法对应的 {@code Method} 实例. {@code Method} 对象的声明类
+     *               将是声明方法的接口, 它可以是代理接口的超接口, 代理类通过该接口继承方法.
+     * @param args   包含在代理实例上的方法调用中传递的参数值的对象数组, 如果接口方法不带参数,
+     *               则为{@code null}. 原始类型的参数包含在适当的原始包装类的实例中, 例如
+     *               {@code java.lang.Integer} 或 {@code java.lang.Boolean}.
+     * @return 从代理实例上的方法调用返回的值. 如果接口方法的声明返回类型是基本类型, 则此方法返回的值必须是相应原始包装类的实例;
+     * 否则, 它必须是可分配给声明的返回类型的类型. 如果此方法返回的值为 {@code null} 且接口方法的返回类型为原始值,
+     * 则代理实例上的方法调用将抛出 {@code NullPointerException}. 如果此方法返回的值与上面描述的接口方法声明的
+     * 返回类型不兼容, 则代理实例上的方法调用将抛出 {@code ClassCastException}.
+     * @throws Throwable 从代理实例上的方法调用中抛出的异常. 异常的类型必须可以分配给接口方法的 {@code throws}
+     *                   子句中声明的任何异常类型, 也可以分配给未经检查的异常类型 {@code java.lang.RuntimeException}
+     *                   或 {@code java.lang.Error}. 如果此方法抛出了一个不能分配给接口方法的 {@code throws} 子句中
+     *                   声明的任何异常类型的已检查异常, 那么包含此方法抛出的异常的 {@link UndeclaredThrowableException}
+     *                   将由代理实例上的方法调用抛出.
      * @see UndeclaredThrowableException
      */
     public Object invoke(Object proxy, Method method, Object[] args)
