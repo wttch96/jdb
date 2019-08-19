f6167a5d8f99a045f28368f00b328ed569f5e664
Wttch[Home-Win10] <admin@wttch.com>
2019-08-19 22:02:33 +0800

    java.lang.ArrayStoreException;

--- src/main/java/java/lang/ArrayStoreException.java
+++ src/main/java/java/lang/ArrayStoreException.java
@@ -1,34 +1,8 @@
-/*
- * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang;
 
 /**
- * Thrown to indicate that an attempt has been made to store the
- * wrong type of object into an array of objects. For example, the
- * following code generates an <code>ArrayStoreException</code>:
+ * 抛出表示尝试将错误类型的对象存储到对象数组中. 例如,
+ * 以下代码生成 <code>ArrayStoreException</code>
  * <blockquote><pre>
  *     Object x[] = new String[3];
  *     x[0] = new Integer(0);
@@ -42,17 +16,16 @@ class ArrayStoreException extends RuntimeException {
     private static final long serialVersionUID = -4522193890499838241L;
 
     /**
-     * Constructs an <code>ArrayStoreException</code> with no detail message.
+     * 构造一个 <code>ArrayStoreException</code> 使用详细信息.
      */
     public ArrayStoreException() {
         super();
     }
 
     /**
-     * Constructs an <code>ArrayStoreException</code> with the specified
-     * detail message.
+     * 构造一个 <code>ArrayStoreException</code> 使用指定的信息.
      *
-     * @param s the detail message.
+     * @param s 详细的信息.
      */
     public ArrayStoreException(String s) {
         super(s);
