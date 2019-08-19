56f11fb878f5b3a4cb05d334d23ee65d4bff3f01
Wttch <admin@wttch.com>
2019-08-19 18:51:30 +0800

    java.lang.reflect.InvocationTargetException;

--- src/main/java/java/lang/reflect/InvocationTargetException.java
+++ src/main/java/java/lang/reflect/InvocationTargetException.java
@@ -1,107 +1,71 @@
-/*
- * Copyright (c) 1996, 2004, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * InvocationTargetException is a checked exception that wraps
- * an exception thrown by an invoked method or constructor.
+ * InvocationTargetException 是一个已检查的异常, 它包装被调用的方法或构造函数抛出的异常.
  *
- * <p>As of release 1.4, this exception has been retrofitted to conform to
- * the general purpose exception-chaining mechanism.  The "target exception"
- * that is provided at construction time and accessed via the
- * {@link #getTargetException()} method is now known as the <i>cause</i>,
- * and may be accessed via the {@link Throwable#getCause()} method,
- * as well as the aforementioned "legacy method."
+ * <p>截止 1.4, 这个异常已经改进过, 以符合通用异常链机制. 在构造是提供并通过
+ * {@link #getTargetException()} 方法访问的 "target exception", 现在称为原因,
+ * 并可以通过 {@link Throwable#getCause()} 以及前述的 "遗留方法" 来访问.
  *
  * @see Method
  * @see Constructor
  */
 public class InvocationTargetException extends ReflectiveOperationException {
     /**
-     * Use serialVersionUID from JDK 1.1.X for interoperability
+     * 使用 jdk 1.1.X 中的 serialVersionUID 实现互操作性
      */
     private static final long serialVersionUID = 4085088731926701167L;
 
     /**
-     * This field holds the target if the
-     * InvocationTargetException(Throwable target) constructor was
-     * used to instantiate the object
+     * 如果使用 InvocationTargetException(Throwable target) 构造函数来实例化对象, 则此字段保存 target
      *
      * @serial
      */
     private Throwable target;
 
     /**
-     * Constructs an {@code InvocationTargetException} with
-     * {@code null} as the target exception.
+     * 构造一个以 {@code null} 为目标异常的 {@code InvocationTargetException}.
      */
     protected InvocationTargetException() {
-        super((Throwable) null);  // Disallow initCause
+        super((Throwable) null);  //禁止 initCause
     }
 
     /**
-     * Constructs a InvocationTargetException with a target exception.
+     * 构造具有目标异常的 InvocationTargetException.
      *
-     * @param target the target exception
+     * @param target 目标异常
      */
     public InvocationTargetException(Throwable target) {
-        super((Throwable) null);  // Disallow initCause
+        super((Throwable) null);  // 禁止 initCause
         this.target = target;
     }
 
     /**
-     * Constructs a InvocationTargetException with a target exception
-     * and a detail message.
+     * 构造具有目标异常和详细信息的 InvocationTargetException.
      *
-     * @param target the target exception
-     * @param s      the detail message
+     * @param target 目标异常
+     * @param s      详细信息
      */
     public InvocationTargetException(Throwable target, String s) {
-        super(s, null);  // Disallow initCause
+        super(s, null);  // 禁止 initCause
         this.target = target;
     }
 
     /**
-     * Get the thrown target exception.
+     * 获得抛出的目标异常.
      *
-     * <p>This method predates the general-purpose exception chaining facility.
-     * The {@link Throwable#getCause()} method is now the preferred means of
-     * obtaining this information.
+     * <p>这种方法早于通用异常链机制. {@link Throwable#getCause()} 方法现在是获取此信息的首选方法.
      *
-     * @return the thrown target exception (cause of this exception).
+     * @return 抛出的目标异常(此异常的原因).
      */
     public Throwable getTargetException() {
         return target;
     }
 
     /**
-     * Returns the cause of this exception (the thrown target exception,
-     * which may be {@code null}).
+     * 返回此异常的原因 (抛出的目标异常, 可以是 {@code null}).
      *
-     * @return the cause of this exception.
+     * @return 异常的原因.
      * @since 1.4
      */
     public Throwable getCause() {
