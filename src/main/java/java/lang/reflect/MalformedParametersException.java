bef0f19cca6811e4879a37cfbc41ed9a99ca7194
Wttch[Home-Win10] <admin@wttch.com>
2019-08-19 21:46:27 +0800

    java.lang.reflect.MalformedParametersException;

--- src/main/java/java/lang/reflect/MalformedParametersException.java
+++ src/main/java/java/lang/reflect/MalformedParametersException.java
@@ -1,45 +1,16 @@
-/*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 /**
- * Thrown when {@link java.lang.reflect.Executable#getParameters the
- * java.lang.reflect package} attempts to read method parameters from
- * a class file and determines that one or more parameters are
- * malformed.
+ * 当 {@link java.lang.reflect.Executable#getParameters java.lang.reflect 包} 尝试
+ * 从类文件中读取方法参数并确定一个或多个参数格式错误时抛出.
  *
- * <p>The following is a list of conditions under which this exception
- * can be thrown:
+ * <p>以下是抛出此异常的条件列表:
  * <ul>
- * <li> The number of parameters (parameter_count) is wrong for the method
- * <li> A constant pool index is out of bounds.
- * <li> A constant pool index does not refer to a UTF-8 entry
- * <li> A parameter's name is "", or contains an illegal character
- * <li> The flags field contains an illegal flag (something other than
- * FINAL, SYNTHETIC, or MANDATED)
+ * <li> 该方法的参数数量(参数计数)是错误的.
+ * <li> 常量池索引超出范围.
+ * <li> 常量池索引不引用 UTF-8 条目.
+ * <li> 参数名称为 "", 或者包含非法字符.
+ * <li> flags 字段包含非法标志(FINAL, SYNTHETIC, 或者 MANDATED 以外的标志)
  * </ul>
  * <p>
  * See {@link java.lang.reflect.Executable#getParameters} for more
@@ -51,21 +22,20 @@ package java.lang.reflect;
 public class MalformedParametersException extends RuntimeException {
 
     /**
-     * Version for serialization.
+     * 序列化版本.
      */
     private static final long serialVersionUID = 20130919L;
 
     /**
-     * Create a {@code MalformedParametersException} with an empty
-     * reason.
+     * 用空原因创建一个 {@code MalformedParametersException} .
      */
     public MalformedParametersException() {
     }
 
     /**
-     * Create a {@code MalformedParametersException}.
+     * 创建一个 {@code MalformedParametersException}.
      *
-     * @param reason The reason for the exception.
+     * @param reason 异常的原因.
      */
     public MalformedParametersException(String reason) {
         super(reason);
