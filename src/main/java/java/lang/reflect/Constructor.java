5e5e766090c0758a05de0b291ed93502db268be0
Wttch <admin@wttch.com>
2019-08-19 18:06:42 +0800

    Constructor 完结撒花 ❁❁❁

--- src/main/java/java/lang/reflect/Constructor.java
+++ src/main/java/java/lang/reflect/Constructor.java
@@ -233,10 +233,8 @@ public final class Constructor<T> extends Executable {
     }
 
     /**
-     * Compares this {@code Constructor} against the specified object.
-     * Returns true if the objects are the same.  Two {@code Constructor} objects are
-     * the same if they were declared by the same class and have the
-     * same formal parameter types.
+     * 将此 {@code Constructor} 和指定的对象进行比较. 如果对象相同则返回 true.
+     * 如果两个 {@code Constructor} 对象由同一个类声明并且具有相同的形参类型, 则他们是相同的.
      */
     public boolean equals(Object obj) {
         if (obj != null && obj instanceof Constructor) {
@@ -249,30 +247,23 @@ public final class Constructor<T> extends Executable {
     }
 
     /**
-     * Returns a hashcode for this {@code Constructor}. The hashcode is
-     * the same as the hashcode for the underlying constructor's
-     * declaring class name.
+     * 返回此 {@code Constructor} 的哈希码. 哈希码与声明类名的底层构造函数的哈希码相同.
      */
     public int hashCode() {
         return getDeclaringClass().getName().hashCode();
     }
 
     /**
-     * Returns a string describing this {@code Constructor}.  The string is
-     * formatted as the constructor access modifiers, if any,
-     * followed by the fully-qualified name of the declaring class,
-     * followed by a parenthesized, comma-separated list of the
-     * constructor's formal parameter types.  For example:
+     * 返回描述此 {@code Constructor} 的字符串. 该字符串被格式化为构造函数访问修饰符(如果有),
+     * 后跟声明类的王权限定名称, 后跟构造函数的形式参数类型的带括号的逗号分割列表. 例如:
      * <pre>
      *    public java.util.Hashtable(int,float)
      * </pre>
      *
-     * <p>The only possible modifiers for constructors are the access
-     * modifiers {@code public}, {@code protected} or
-     * {@code private}.  Only one of these may appear, or none if the
-     * constructor has default (package) access.
+     * <p>构造函数唯一可能的修饰符是访问修饰符 {@code public}, {@code protected} 或 {@code private}.
+     * 如果构造函数具有默认(包)访问权限, 则只能显示其中一个, 或者不显示.
      *
-     * @return a string describing this {@code Constructor}
+     * @return 描述此 {@code Constructor} 的字符串.
      * @jls 8.8.3. Constructor Modifiers
      */
     public String toString() {
@@ -288,35 +279,21 @@ public final class Constructor<T> extends Executable {
     }
 
     /**
-     * Returns a string describing this {@code Constructor},
-     * including type parameters.  The string is formatted as the
-     * constructor access modifiers, if any, followed by an
-     * angle-bracketed comma separated list of the constructor's type
-     * parameters, if any, followed by the fully-qualified name of the
-     * declaring class, followed by a parenthesized, comma-separated
-     * list of the constructor's generic formal parameter types.
+     * 返回描述此 {@code Constructor} 的字符串, 包括类型参数. 字符串被格式化为构造函数访问修饰符(如果有),
+     * 后跟一个以尖括号括起来的逗号分割的构造函数参数类型列表(如果有), 后跟声明类的完全限定名称, 后跟带括号的
+     * 逗号分隔的构造函数泛型类型的参数类型列表.
      * <p>
-     * If this constructor was declared to take a variable number of
-     * arguments, instead of denoting the last parameter as
-     * "<tt><i>Type</i>[]</tt>", it is denoted as
-     * "<tt><i>Type</i>...</tt>".
+     * 如果声明此构造函数采用可变数量的参数, 而不是将最后一个参数表示为
+     * "<tt><i>Type</i>[]</tt>", 则将其表示为 "<tt><i>Type</i>...</tt>".
      * <p>
-     * A space is used to separate access modifiers from one another
-     * and from the type parameters or return type.  If there are no
-     * type parameters, the type parameter list is elided; if the type
-     * parameter list is present, a space separates the list from the
-     * class name.  If the constructor is declared to throw
-     * exceptions, the parameter list is followed by a space, followed
-     * by the word "{@code throws}" followed by a
-     * comma-separated list of the thrown exception types.
+     * 空格用于将访问修饰符彼此分开, 并与类型参数或返回类型分开. 如果没有类型参数, 则省略类型参数列表;
+     * 如果存在类型参数列表, 则空格将从类名称中分离列表. 如果声明构造函数抛出异常, 则参数列表后跟一个空格,
+     * 后跟单词 "{@code throws}", 后跟逗号分割的抛出异常类型列表.
      *
-     * <p>The only possible modifiers for constructors are the access
-     * modifiers {@code public}, {@code protected} or
-     * {@code private}.  Only one of these may appear, or none if the
-     * constructor has default (package) access.
+     * <p>构造函数唯一可能的修饰符是访问修饰符 {@code public}, {@code protected} 或 {@code private}.
+     * 如果构造函数具有默认(包)访问权限, 则只能显示其中一个, 或者不显示.
      *
-     * @return a string describing this {@code Constructor},
-     * include type parameters
+     * @return 描述此 {@code Constructor} 的字符串, 包括类型参数.
      * @jls 8.8.3. Constructor Modifiers
      * @since 1.5
      */
@@ -331,50 +308,29 @@ public final class Constructor<T> extends Executable {
     }
 
     /**
-     * Uses the constructor represented by this {@code Constructor} object to
-     * create and initialize a new instance of the constructor's
-     * declaring class, with the specified initialization parameters.
-     * Individual parameters are automatically unwrapped to match
-     * primitive formal parameters, and both primitive and reference
-     * parameters are subject to method invocation conversions as necessary.
+     * 使用此 {@code Constructor} 对象表示的构造函数, 使用指定的初始化参数创建和初始化构造函数声明类的新实例.
+     * 各个参数自动解包以匹配原始类型参数, 并且原始参数和参考参数都根据需要进行调整转换.
      *
-     * <p>If the number of formal parameters required by the underlying constructor
-     * is 0, the supplied {@code initargs} array may be of length 0 or null.
+     * <p>如果底层的构造函数所需的形参的数量为 0, 则提供的 {@code initargs} 数组的长度可以为 0 或者  null.
      *
-     * <p>If the constructor's declaring class is an inner class in a
-     * non-static context, the first argument to the constructor needs
-     * to be the enclosing instance; see section 15.9.3 of
-     * <cite>The Java&trade; Language Specification</cite>.
+     * <p>如果构造函数的声明类是非静态上下文中的内部类, 则构造函数的第一个参数需要是封闭实例;
+     * 参见 <cite>The Java&trade; Language Specification</cite> 15.9.3 节.
      *
-     * <p>If the required access and argument checks succeed and the
-     * instantiation will proceed, the constructor's declaring class
-     * is initialized if it has not already been initialized.
+     * <p>如果所需的访问和参数检查成功并实例化将继续, 则构造函数的声明类如果尚未初始化则初始化.
      *
-     * <p>If the constructor completes normally, returns the newly
-     * created and initialized instance.
+     * <p>如果构造函数正常完成, 则返回新创建和初始化的实例.
      *
-     * @param initargs array of objects to be passed as arguments to
-     *                 the constructor call; values of primitive types are wrapped in
-     *                 a wrapper object of the appropriate type (e.g. a {@code float}
-     *                 in a {@link java.lang.Float Float})
-     * @return a new object created by calling the constructor
-     * this object represents
-     * @throws IllegalAccessException      if this {@code Constructor} object
-     *                                     is enforcing Java language access control and the underlying
-     *                                     constructor is inaccessible.
-     * @throws IllegalArgumentException    if the number of actual
-     *                                     and formal parameters differ; if an unwrapping
-     *                                     conversion for primitive arguments fails; or if,
-     *                                     after possible unwrapping, a parameter value
-     *                                     cannot be converted to the corresponding formal
-     *                                     parameter type by a method invocation conversion; if
-     *                                     this constructor pertains to an enum type.
-     * @throws InstantiationException      if the class that declares the
-     *                                     underlying constructor represents an abstract class.
-     * @throws InvocationTargetException   if the underlying constructor
-     *                                     throws an exception.
-     * @throws ExceptionInInitializerError if the initialization provoked
-     *                                     by this method fails.
+     * @param initargs 要作为参数传递给构造函数调用的对象数组; 原始类型的值包装在适当类型的自动装箱对象中
+     *                 (例如:{@code float} 装箱为 {@link java.lang.Float Float})
+     * @return 通过调用此对象表示的构造函数创建的新对象
+     * @throws IllegalAccessException      如果此 {@code Constructor} 对象强制执行 java 语言访问控制且
+     *                                     基础构造函数不可访问.
+     * @throws IllegalArgumentException    如果实际参数和类型参数的数量不同; 如果原始参数的展开转换失败; 或者,
+     *                                     如果在可能的解包之后, 参数不能通过方法调用转换为相应的形式参数类型;
+     *                                     如果此构造函数输入枚举类型.
+     * @throws InstantiationException      如果声明底层构造函数的类是抽象类.
+     * @throws InvocationTargetException   如果底层构造函数抛出了异常.
+     * @throws ExceptionInInitializerError 如果此方法引发的初始化失败.
      */
     @CallerSensitive
     public T newInstance(Object... initargs)
@@ -386,6 +342,7 @@ public final class Constructor<T> extends Executable {
                 checkAccess(caller, clazz, null, modifiers);
             }
         }
+        // 枚举类型
         if ((clazz.getModifiers() & Modifier.ENUM) != 0)
             throw new IllegalArgumentException("Cannot reflectively create enum objects");
         ConstructorAccessor ca = constructorAccessor;   // read volatile
@@ -418,20 +375,17 @@ public final class Constructor<T> extends Executable {
         return super.isSynthetic();
     }
 
-    // NOTE that there is no synchronization used here. It is correct
-    // (though not efficient) to generate more than one
-    // ConstructorAccessor for a given Constructor. However, avoiding
-    // synchronization will probably make the implementation more
-    // scalable.
+    // 请注意, 此处未使用同步. 为给定的 Constructor 生成多个 ConstructorAccessor 是正确的(尽管效率不高).
+    // 但是, 避免同步可能会使实现更具可伸缩性.
     private ConstructorAccessor acquireConstructorAccessor() {
-        // First check to see if one has been created yet, and take it
-        // if so.
+        // 首先检查一下是否已经创建了一个, 如果是的话
         ConstructorAccessor tmp = null;
+        // 共享 root 的 ConstructorAccessor
         if (root != null) tmp = root.getConstructorAccessor();
         if (tmp != null) {
             constructorAccessor = tmp;
         } else {
-            // Otherwise fabricate one and propagate it up to the root
+            // 否则制作一个并将其传播到根目录
             tmp = reflectionFactory.newConstructorAccessor(this);
             setConstructorAccessor(tmp);
         }
@@ -439,17 +393,15 @@ public final class Constructor<T> extends Executable {
         return tmp;
     }
 
-    // Returns ConstructorAccessor for this Constructor object, not
-    // looking up the chain to the root
+    // 返回此 Constructor 对象的构造函数访问器, 而不是查找到根的链
     ConstructorAccessor getConstructorAccessor() {
         return constructorAccessor;
     }
 
-    // Sets the ConstructorAccessor for this Constructor object and
-    // (recursively) its root
+    // 为此 Constructor 对象设置 ConstructorAccessor, 并(递归地)设置其根
     void setConstructorAccessor(ConstructorAccessor accessor) {
         constructorAccessor = accessor;
-        // Propagate up
+        // 传播到根
         if (root != null) {
             root.setConstructorAccessor(accessor);
         }
@@ -504,14 +456,14 @@ public final class Constructor<T> extends Executable {
     @Override
     void handleParameterNumberMismatch(int resultLength, int numParameters) {
         Class<?> declaringClass = getDeclaringClass();
+        // 枚举, 匿名类, 局部类
         if (declaringClass.isEnum() ||
                 declaringClass.isAnonymousClass() ||
                 declaringClass.isLocalClass())
-            return; // Can't do reliable parameter counting
+            return; // 不能做可靠的参数计数
         else {
-            if (!declaringClass.isMemberClass() || // top-level
-                    // Check for the enclosing instance parameter for
-                    // non-static member classes
+            if (!declaringClass.isMemberClass() || // 顶层
+                    // 检查非静态成员类的封闭实例参数
                     (declaringClass.isMemberClass() &&
                             ((declaringClass.getModifiers() & Modifier.STATIC) == 0) &&
                             resultLength + 1 != numParameters)) {
