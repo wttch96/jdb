d7e37d1d26c6ae36afbcfbd2697199a6c794d9cf
Wttch[Home-Win10] <admin@wttch.com>
2019-08-29 00:28:17 +0800

    java.lang.reflect.ReflectAccess;

--- src/main/java/java/lang/reflect/ReflectAccess.java
+++ src/main/java/java/lang/reflect/ReflectAccess.java
@@ -1,37 +1,10 @@
-/*
- * Copyright (c) 2001, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.reflect;
 
 import sun.reflect.MethodAccessor;
 import sun.reflect.ConstructorAccessor;
 
 /**
- * Package-private class implementing the
- * sun.reflect.LangReflectAccess interface, allowing the java.lang
- * package to instantiate objects in this package.
+ * 包私有类实现 sun.reflect.LangReflectAccess 接口, 允许 java.lang 包在此包中实例化对象.
  */
 
 class ReflectAccess implements sun.reflect.LangReflectAccess {
