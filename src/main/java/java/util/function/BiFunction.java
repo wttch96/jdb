90a07228f40176d4d71271c2a8846a07344e89d9
wttch <wttch@wttch.com>
2020-06-30 21:25:57 +0800

    java.util.function.BiFunction;(T, U) -> R

--- src/main/java/java/util/function/BiFunction.java
+++ src/main/java/java/util/function/BiFunction.java
@@ -1,34 +1,9 @@
-/*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package java.util.function;
 
 import java.util.Objects;
 
 /**
- * Represents a function that accepts two arguments and produces a result.
- * This is the two-arity specialization of {@link Function}.
+ * 表示一个接受两个参数并产生结果的函数. 这是{@link Function}的两元特殊化.
  *
  * <p>This is a <a href="package-summary.html">functional interface</a>
  * whose functional method is {@link #apply(Object, Object)}.
@@ -53,8 +28,11 @@ public interface BiFunction<T, U, R> {
     R apply(T t, U u);
 
     /**
-     * Returns a composed function that first applies this function to
-     * its input, and then applies the {@code after} function to the result.
+     * 返回一个组合函数，该函数首先将此函数应用于输入, 然后将{@code after}函数应用于结果.
+     * this:                (T,U) -> R
+     * after:               (R) -> V
+     * this.andThen(after): (T,U) -> V
+     *
      * If evaluation of either function throws an exception, it is relayed to
      * the caller of the composed function.
      *
