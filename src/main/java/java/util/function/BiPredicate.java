2882c58453a9ed6488f1bcb2d337936f484ddae9
wttch <wttch@wttch.com>
2020-06-30 21:48:34 +0800

    java.util.function.BiPredicate;(T, U) -> boolean

--- src/main/java/java/util/function/BiPredicate.java
+++ src/main/java/java/util/function/BiPredicate.java
@@ -1,34 +1,9 @@
-/*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package java.util.function;
 
 import java.util.Objects;
 
 /**
- * Represents a predicate (boolean-valued function) of two arguments.  This is
- * the two-arity specialization of {@link Predicate}.
+ * 表示两个参数的断言(布尔值函数). 这是 {@link Predicate}的两元特殊化形式.
  *
  * <p>This is a <a href="package-summary.html">functional interface</a>
  * whose functional method is {@link #test(Object, Object)}.
@@ -43,7 +18,7 @@ import java.util.Objects;
 public interface BiPredicate<T, U> {
 
     /**
-     * Evaluates this predicate on the given arguments.
+     * 根据给定参数评估此断言.
      *
      * @param t the first input argument
      * @param u the second input argument
