653c9448d56f14fbdf3ee439e9a2547f5fa7fa7d
wttch <wttch@wttch.com>
2020-12-13 19:18:43 +0800

    java.util.stream.StreamShape;

--- src/main/java/java/util/stream/StreamShape.java
+++ src/main/java/java/util/stream/StreamShape.java
@@ -1,70 +1,32 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package java.util.stream;
 
 /**
- * An enum describing the known shape specializations for stream abstractions.
- * Each will correspond to a specific subinterface of {@link BaseStream}
- * (e.g., {@code REFERENCE} corresponds to {@code Stream}, {@code INT_VALUE}
- * corresponds to {@code IntStream}).  Each may also correspond to
- * specializations of value-handling abstractions such as {@code Spliterator},
- * {@code Consumer}, etc.
+ * 一个描述流抽象的已知形状专业的枚举. 每个将对应于{@link BaseStream}的特定子接口
+ * (例如, {@code REFERENCE}对应于{@code Stream}, {@code INT_VALUE}对应于{@code IntStream}).
+ * 每个也可能对应于值处理抽象的专业化, 例如{@code Spliterator}, {@code Consumer}等.
  *
- * @apiNote
- * This enum is used by implementations to determine compatibility between
- * streams and operations (i.e., if the output shape of a stream is compatible
- * with the input shape of the next operation).
+ * @apiNote 实现使用该枚举来确定流和操作之间的兼容性(即, 流的输出形状是否与下一个操作的输入形状兼容).
  *
- * <p>Some APIs require you to specify both a generic type and a stream shape
- * for input or output elements, such as {@link TerminalOp} which has both
- * generic type parameters for its input types, and a getter for the
- * input shape.  When representing primitive streams in this way, the
- * generic type parameter should correspond to the wrapper type for that
- * primitive type.
- *
+ * <p>某些API要求您为输入或输出元素同时指定通用类型和流形状, 例如{@link TerminalOp},
+ * 其输入类型既具有通用类型参数, 又具有输入形状的吸气剂. 当以这种方式表示原始流时,
+ * 泛型类型参数应对应于该原始类型的包装器类型.
  * @since 1.8
  */
 enum StreamShape {
     /**
-     * The shape specialization corresponding to {@code Stream} and elements
-     * that are object references.
+     * 与{@code Stream}和作为对象引用的元素相对应的形状特化.
      */
     REFERENCE,
     /**
-     * The shape specialization corresponding to {@code IntStream} and elements
-     * that are {@code int} values.
+     * 对应于{@code IntStream}和{@code int}值元素的形状特化.
      */
     INT_VALUE,
     /**
-     * The shape specialization corresponding to {@code LongStream} and elements
-     * that are {@code long} values.
+     * 对应于{@code LongStream}和{@code long}值元素的形状特化.
      */
     LONG_VALUE,
     /**
-     * The shape specialization corresponding to {@code DoubleStream} and
-     * elements that are {@code double} values.
+     * 对应于{@code DoubleStream}和{@code double}值元素的形状特化.
      */
     DOUBLE_VALUE
 }
