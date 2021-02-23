16ff31ec818836fe0e145686a1800eec53c8270e
wttch <wttch@wttch.com>
2021-02-23 18:19:34 +0800

    java.io.InterruptedIOException;

--- src/main/java/java/io/InterruptedIOException.java
+++ src/main/java/java/io/InterruptedIOException.java
@@ -1,74 +1,48 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.io;
 
 /**
- * Signals that an I/O operation has been interrupted. An
- * <code>InterruptedIOException</code> is thrown to indicate that an
- * input or output transfer has been terminated because the thread
- * performing it was interrupted. The field {@link #bytesTransferred}
- * indicates how many bytes were successfully transferred before
- * the interruption occurred.
+ * 表示IO操作已中断. 抛出<code>InterruptedIOException<code>表示输入或输出传输已终止, 因为执行该输入或输出的线程已中断. 字段{@link
+ * #bytesTransferred}指示中断发生之前成功传输了多少字节.
+ * <p>
+ * wttch: 一般是在 io 操作中 wait() 等方法调用时, 出现 {@link InterruptedException} 时包装 InterruptedException.
  *
- * @author  unascribed
- * @see     java.io.InputStream
- * @see     java.io.OutputStream
- * @see     java.lang.Thread#interrupt()
- * @since   JDK1.0
+ * @author unascribed
+ * @see java.io.InputStream
+ * @see java.io.OutputStream
+ * @see java.lang.Thread#interrupt()
+ * @since JDK1.0
  */
 public
 class InterruptedIOException extends IOException {
-    private static final long serialVersionUID = 4020568460727500567L;
 
-    /**
-     * Constructs an <code>InterruptedIOException</code> with
-     * <code>null</code> as its error detail message.
-     */
-    public InterruptedIOException() {
-        super();
-    }
+  private static final long serialVersionUID = 4020568460727500567L;
 
-    /**
-     * Constructs an <code>InterruptedIOException</code> with the
-     * specified detail message. The string <code>s</code> can be
-     * retrieved later by the
-     * <code>{@link java.lang.Throwable#getMessage}</code>
-     * method of class <code>java.lang.Throwable</code>.
-     *
-     * @param   s   the detail message.
-     */
-    public InterruptedIOException(String s) {
-        super(s);
-    }
+  /**
+   * Constructs an <code>InterruptedIOException</code> with
+   * <code>null</code> as its error detail message.
+   */
+  public InterruptedIOException() {
+    super();
+  }
 
-    /**
-     * Reports how many bytes had been transferred as part of the I/O
-     * operation before it was interrupted.
-     *
-     * @serial
-     */
-    public int bytesTransferred = 0;
+  /**
+   * Constructs an <code>InterruptedIOException</code> with the specified detail message. The
+   * string
+   * <code>s</code> can be retrieved later by the
+   * <code>{@link java.lang.Throwable#getMessage}</code>
+   * method of class <code>java.lang.Throwable</code>.
+   *
+   * @param s the detail message.
+   */
+  public InterruptedIOException(String s) {
+    super(s);
+  }
+
+  /**
+   * Reports how many bytes had been transferred as part of the I/O operation before it was
+   * interrupted.
+   *
+   * @serial
+   */
+  public int bytesTransferred = 0;
 }
